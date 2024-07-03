2e9518468d6564a3604c2ee3edf0bb6057bfe46c
seoAreum <1971236@hansung.ac.kr>
2024-07-04 04:29:51 +0900

    style: eslint 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,29 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'prettier',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh','prettier'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+module.exports = [
+  {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react/recommended',
+      'prettier',
     ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier'],
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          'react/jsx-props-no-spreading': 'off',
+          'eol-last': ['error', 'always'],
+          'react/react-in-jsx-scope': 'off',
+          'no-multi-spaces': 'error',
+          'simple-import-sort/imports': 'error',
+          allowConstantExport: true,
+        },
+      ],
+    },
   },
-}
+];
