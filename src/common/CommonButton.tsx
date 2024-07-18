cd63eaec4a56aa3c83f03124534a8444afd5d976
Seung taek Oh <elqkddl@naver.com>
2024-07-18 19:16:34 +0900

    feat: 그룹생성하기 그룹바로가기 분기처리

--- src/common/CommonButton.tsx
+++ src/common/CommonButton.tsx
@@ -2,18 +2,53 @@ import styled from 'styled-components';
 
 interface CommonButtonProps {
   isActive: boolean;
+  category: string;
 }
 
-const CommonButton = ({ isActive }: CommonButtonProps) => {
+const CommonButton = ({ isActive, category }: CommonButtonProps) => {
+  const getText = () => {
+    switch (category) {
+      case 'create':
+        return '그룹 생성하기';
+      case 'join':
+        return '그룹 바로가기';
+      default:
+        return '가입하기';
+    }
+  };
+
   return (
     <>
-      <Button isActive={isActive}>가입하기</Button>
+      <Button $category={category} isActive={isActive}>
+        {getText()}
+      </Button>
     </>
   );
 };
 
-const Button = styled.button<CommonButtonProps>`
-  padding: 1.2rem 5.4rem;
+export default CommonButton;
+
+const Button = styled.button<{ $category: string; isActive: boolean }>`
+  width: ${({ $category }) => {
+    switch ($category) {
+      case 'create':
+        return '14.2rem';
+      case 'join':
+        return '17.6rem';
+      default:
+        return '17.8rem';
+    }
+  }};
+  padding: ${({ $category }) => {
+    switch ($category) {
+      case 'create':
+        return '1rem 1.4rem';
+      case 'join':
+        return '1rem 4.7rem 1rem 2rem';
+      default:
+        return '1.2rem 1.4rem';
+    }
+  }};
 
   border-radius: 0.8rem;
   background-color: ${({ theme, isActive }) =>
@@ -21,5 +56,3 @@ const Button = styled.button<CommonButtonProps>`
   color: ${({ theme, isActive }) =>
     isActive ? theme.colors.gray900 : theme.colors.gray300};
 `;
-
-export default CommonButton;
