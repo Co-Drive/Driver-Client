a37c6c84c301c75bcebf44fed1802e935fb6b887
seoAreum <1971236@hansung.ac.kr>
2024-07-18 16:55:52 +0900

    chore: 타입 분리

--- src/common/CommonTextarea.tsx
+++ src/common/CommonTextarea.tsx
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { PLACEHOLDER } from '../constants/CommonTextarea/textareaConst';
 import { CommonTextareaProps } from '../types/\bCommonTextarea/TextareaType';
 
 const CommonTextarea = ({
@@ -6,10 +7,6 @@ const CommonTextarea = ({
   value,
   handleChangeTextarea,
 }: CommonTextareaProps) => {
-  const PLACEHOLDER = [
-    '그룹에 대해서 간단하게 설명해주세요',
-    '진행 방식을 설명해주세요',
-  ];
   const isGroupType = category === 'group';
 
   return (
