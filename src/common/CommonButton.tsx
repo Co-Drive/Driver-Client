8260e3b79f0d825ca21fe8ccee814d8be82d36b1
Seung taek Oh <elqkddl@naver.com>
2024-07-19 15:50:55 +0900

    feat: Button(상수,map)추가 및 IconContainer,BtnText 스타일 변경

--- src/common/CommonButton.tsx
+++ src/common/CommonButton.tsx
@@ -1,98 +1,96 @@
 import styled from 'styled-components';
-import { IcAdd, IcArrowRightGray, IcBtnCopy } from '../assets';
+import { IcAddBlack, IcAddGray, IcArrowRightBlack, IcBtnCopy } from '../assets';
 
 interface CommonButtonProps {
   isActive?: boolean;
   category: string;
 }
 
-const CommonButton = ({ isActive = false, category }: CommonButtonProps) => {
-  const getText = () => {
-    switch (category) {
-      case 'group_create':
-        return '그룹 생성하기';
-      case 'group_join':
-        return '그룹 바로가기';
-      case 'account_create':
-        return '가입하기';
-      case 'link_copy':
-        return '링크 복사하기';
-    }
-  };
+const CONTENTS = [
+  {
+    category: 'group_create',
+    text: '그룹 생성하기',
+    icon_active: <IcAddBlack />,
+    icon_disabled: <IcAddGray />,
+  },
+  {
+    category: 'group_direct',
+    text: '그룹 바로가기',
+    icon: <IcArrowRightBlack />,
+  },
+  { category: 'account_create', text: '가입하기' },
+  { category: 'link_copy', text: '링크 복사하기', icon: <IcBtnCopy /> },
+  { category: 'group_join', text: '참여하기' },
+];
 
-  const getIcon = () => {
-    switch (category) {
-      case 'group_create':
-        return IcAdd;
-      case 'group_join':
-        return IcArrowRightGray;
-      case 'link_copy':
-        return IcBtnCopy;
-      default:
-        return null;
-    }
-  };
-
-  // const IconComponent = getIcon();
-
+const CommonButton = ({ isActive, category }: CommonButtonProps) => {
   return (
-    <>
-      <Button $category={category} $isActive={isActive}>
-        {category === 'link_copy' ? (
-          <LogoCopyContainer>
-            {getText()}
-            <Icon as={IcBtnCopy} />
-          </LogoCopyContainer>
-        ) : (
-          getText()
-        )}
-      </Button>
-    </>
+    <Button $category={category} $isActive={isActive}>
+      {CONTENTS.map((content) => {
+        return (
+          category === content.category && (
+            <>
+              {category === 'group_create' && (
+                <IconContainer>
+                  {isActive ? content.icon_active : content.icon_disabled}
+                </IconContainer>
+              )}
+              <BtnText $category={category} $isActive={isActive}>
+                {content.text}
+              </BtnText>
+              {(category === 'group_direct' || category === 'link_copy') && (
+                <IconContainer>{content.icon}</IconContainer>
+              )}
+            </>
+          )
+        );
+      })}
+    </Button>
   );
 };
 
 export default CommonButton;
 
-const LogoCopyContainer = styled.div`
+const Button = styled.button<{ $category: string; $isActive?: boolean }>`
   display: flex;
+  gap: 0.8rem;
   justify-content: center;
   align-items: center;
-
-  /* background-color: pink; */
-`;
-
-const Button = styled.button<{ $category: string; $isActive: boolean }>`
-  display: flex;
-  justify-content: center;
-
-  ${({ theme }) => theme.fonts.title_bold_20}
 
   padding: ${({ $category }) => {
     switch ($category) {
       case 'group_create':
-        return '1rem 1.4rem 1rem 4rem';
-      case 'group_join':
-        return '1rem 4.7rem 1rem 2rem';
-      case 'account_create':
-        return '1.2rem 5.4rem';
+        return '1rem 1.4rem';
+      case 'group_direct':
+        return '1.4rem 1.8rem 1.4rem 2.1rem';
       case 'link_copy':
         return '1rem 2rem';
+      default:
+        return '1.2rem 5.4rem';
     }
   }};
 
   border-radius: 0.8rem;
   background-color: ${({ theme, $isActive, $category }) => {
-    if ($category === 'link_copy') return theme.colors.gray700;
-    return $isActive ? theme.colors.codrive_green : theme.colors.gray700;
-  }};
-  color: ${({ theme, $isActive, $category }) => {
-    if ($category === 'link_copy') return theme.colors.white;
-    return $isActive ? theme.colors.gray900 : theme.colors.white;
+    if ($isActive || $category === 'group_direct')
+      return theme.colors.codrive_green;
+    return theme.colors.gray700;
   }};
 `;
 
-const Icon = styled.svg`
-  margin-left: 0.8rem;
+const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-  background-color: pink;
+const BtnText = styled.p<{ $category: string; $isActive?: boolean }>`
+  ${({ theme, $category }) =>
+    $category === 'group_direct'
+      ? theme.fonts.title_bold_16
+      : theme.fonts.title_bold_20}
+  color: ${({ theme, $isActive, $category }) => {
+    if ($isActive || $category === 'group_direct') return theme.colors.gray900;
+    return theme.colors.white;
+  }};
 `;
