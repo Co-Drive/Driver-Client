d94cf7728fe8ff94b681bb2b5ab80c7080d8b4a5
Seung taek Oh <elqkddl@naver.com>
2024-07-19 15:54:21 +0900

    chore: CommonButton 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components';
-import CommonButton from './common/CommonButton';
 import Router from './Router';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
@@ -9,11 +8,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
-      <CommonButton isActive={true} category="account_create" />
-      <CommonButton isActive={false} category="group_create" />
-      <CommonButton isActive={false} category="group_direct" />
-      <CommonButton category="link_copy" />
-      <CommonButton isActive={false} category="group_join" />
     </ThemeProvider>
   );
 }
