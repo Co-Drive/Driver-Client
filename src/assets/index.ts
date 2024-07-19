59ef7009de2e5dd354d44557c40661a54192b494
seoAreum <1971236@hansung.ac.kr>
2024-07-19 16:49:39 +0900

    style: 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,5 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import BtnGroupLock from './btn/btn_group_lock.svg?react';
-
 import IcAdd from './icon/ic_add.svg?react';
 import IcArrowBottomGray from './icon/ic_arrow_bottom_gray.svg?react';
 import IcArrowLeftFill from './icon/ic_arrow_left_fill.svg?react';
@@ -15,12 +13,16 @@ import IcCancelSmall from './icon/ic_cancel_small.svg?react';
 import IcGroup from './icon/ic_group.svg?react';
 import IcHome from './icon/ic_home.svg?react';
 import IcLogo from './icon/ic_logo.svg?react';
+import IcSecretBigWhite from './icon/ic_secret_big_white.svg?react';
+import IcSecretGray from './icon/ic_secret_gray.svg?react';
+import IcSecretWhite from './icon/ic_secret_white.svg?react';
 import IcSuccess from './icon/ic_success.svg?react';
+import IcUnlockGray from './icon/ic_unlock_gray.svg?react';
+import IcUnlockWhite from './icon/ic_unlock_white.svg?react';
 import IcWorkbook from './icon/ic_workbook.svg?react';
 
 // svg 더 추가하게 되면 ,(콤마)로 이어서 쓰기
 export {
-  BtnGroupLock,
   IcAdd,
   IcArrowBottomGray,
   IcArrowLeftFill,
@@ -34,6 +36,11 @@ export {
   IcGroup,
   IcHome,
   IcLogo,
+  IcSecretBigWhite,
+  IcSecretGray,
+  IcSecretWhite,
   IcSuccess,
+  IcUnlockGray,
+  IcUnlockWhite,
   IcWorkbook,
 };
