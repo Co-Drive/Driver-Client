6d1546948d7685e9e94aba7cfc079ec2a566c605
seoAreum <1971236@hansung.ac.kr>
2024-07-22 17:04:50 +0900

    feat: 코드 에디터 초기 구현

---
+++ src/components/Solve/CodeEditor.tsx
@@ -0,0 +1,87 @@
+import { cpp } from '@codemirror/lang-cpp';
+import { java } from '@codemirror/lang-java';
+import { javascript } from '@codemirror/lang-javascript';
+import { python } from '@codemirror/lang-python';
+import { StreamLanguage } from '@codemirror/language';
+import { c, kotlin, scala } from '@codemirror/legacy-modes/mode/clike';
+import { go } from '@codemirror/legacy-modes/mode/go';
+import { ruby } from '@codemirror/legacy-modes/mode/ruby';
+import { swift } from '@codemirror/legacy-modes/mode/swift';
+import { csharp } from '@replit/codemirror-lang-csharp';
+import { dracula } from '@uiw/codemirror-theme-dracula';
+import CodeMirror, { EditorView } from '@uiw/react-codemirror';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const CodeEditor = () => {
+  const [code, setCode] = useState(`import react from "react"`);
+
+  // 나중에 state로 만들어서 유동적으로 바뀌게 할 예정
+  const LANGUAGE = 'javascript';
+
+  // 상수 파일로 분리할 예정
+  const LANG_LIST = {
+    c: () => StreamLanguage.define(c),
+    csharp: () => csharp(),
+    scala: () => StreamLanguage.define(scala),
+    kotlin: () => StreamLanguage.define(kotlin),
+    java,
+    javascript,
+    python,
+    cpp,
+    go: () => StreamLanguage.define(go),
+    swift: () => StreamLanguage.define(swift),
+    ruby: () => StreamLanguage.define(ruby),
+  };
+
+  // extends에 항상 배열이 들어가도록 배열을 반환하는 함수
+  const getExtensions = (language: keyof typeof LANG_LIST) => {
+    // langSupport: 함수 반환
+    const langSupport = LANG_LIST[language];
+    // extension에 EditorView.lineWrapping을 추가해서 CodeMirror가 줄바꿈을 지원하도록 함
+    const extension = [langSupport && langSupport(), EditorView.lineWrapping];
+
+    return extension;
+  };
+
+  const extensions = getExtensions(LANGUAGE);
+
+  const handleChangeCode = (newCode: string) => {
+    setCode(newCode);
+  };
+
+  return (
+    <CodeMirrorWrapper className="code-mirror">
+      <CodeMirror
+        value={code}
+        onChange={handleChangeCode}
+        extensions={extensions}
+        theme={dracula}
+        minHeight="38.2rem"
+      />
+    </CodeMirrorWrapper>
+  );
+};
+
+export default CodeEditor;
+
+const CodeMirrorWrapper = styled.article`
+  overflow: hidden auto;
+
+  width: 92.6rem;
+  min-height: 38.2rem;
+
+  border-radius: 0.8rem;
+
+  .cm-editor {
+    background-color: #23262e;
+  }
+
+  /* 전체 코드 스페이스 */
+  .cm-scroller {
+    word-break: break-all;
+
+    white-space: pre-wrap;
+    overflow-wrap: break-word;
+  }
+`;
