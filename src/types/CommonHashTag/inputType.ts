30404bfe2f69fc70d1710b2aec97342acbf8b850
kimmoonju-102 <kimmun3549@naver.com>
2024-07-23 16:30:56 +0900

    feat: 타입 분리

--- src/types/CommonHashTag/inputType.ts
+++ src/types/CommonHashTag/inputType.ts
@@ -1,6 +1,4 @@
 export interface CommonHashTagProps {
   selectedTag: string;
-  inputValue: string;
-  onTagChange: (tag: string) => void;
-  onInputChange: (value: string) => void;
+  removeTag: () => void;
 }
