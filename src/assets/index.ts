5fee3613a5def8a532237d937a3cb671473bf5b2
seoAreum <1971236@hansung.ac.kr>
2024-07-23 11:13:16 +0900

    style: 에셋 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -13,12 +13,18 @@ import IcArrowTopGray from './icon/ic_arrow_top_gray.svg?react';
 import IcBtnCopy from './icon/ic_btn_copy.svg?react';
 import IcBtnInformation from './icon/ic_btn_information.svg?react';
 import IcCancelSmall from './icon/ic_cancel_small.svg?react';
+import IcCode from './icon/ic_code.svg?react';
 import IcGroup from './icon/ic_group.svg?react';
 import IcHome from './icon/ic_home.svg?react';
+import IcInformation from './icon/ic_information.svg?react';
+import IcLinkGray from './icon/ic_link_gray.svg?react';
 import IcLogo from './icon/ic_logo.svg?react';
+import IcMemoWhite from './icon/ic_memo_white.svg?react';
 import IcSecretBigWhite from './icon/ic_secret_big_white.svg?react';
 import IcSecretGray from './icon/ic_secret_gray.svg?react';
 import IcSecretWhite from './icon/ic_secret_white.svg?react';
+import IcStarGray from './icon/ic_star_gray.svg?react';
+import IcStarGreen from './icon/ic_star_green.svg?react';
 import IcSuccess from './icon/ic_success.svg?react';
 import IcUnlockGray from './icon/ic_unlock_gray.svg?react';
 import IcUnlockWhite from './icon/ic_unlock_white.svg?react';
@@ -39,12 +45,18 @@ export {
   IcBtnCopy,
   IcBtnInformation,
   IcCancelSmall,
+  IcCode,
   IcGroup,
   IcHome,
+  IcInformation,
+  IcLinkGray,
   IcLogo,
+  IcMemoWhite,
   IcSecretBigWhite,
   IcSecretGray,
   IcSecretWhite,
+  IcStarGray,
+  IcStarGreen,
   IcSuccess,
   IcUnlockGray,
   IcUnlockWhite,
