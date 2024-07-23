0616fa3bd4791bd8010a057b73d2d7193db9a029
Seung taek Oh <elqkddl@naver.com>
2024-07-24 03:18:18 +0900

    feat: category만 props로 넘겨주며 PageLayout 생성

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Header from '../../common/Header';
 
 interface PageLayoutProps {
+  category: string;
   children: React.ReactNode;
 }
 
-const PageLayout = ({ children }: PageLayoutProps) => {
-  const isLogin = true;
-  const [clickedCategory, setClickedCategory] = useState(isLogin ? '홈' : '');
+const PageLayout = ({ category, children }: PageLayoutProps) => {
+  const [clickedCategory, setClickedCategory] = useState(category);
 
   const handleClickCategory = (
     e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
   ) => {
-    setClickedCategory(e.currentTarget.innerHTML);
+    const { innerHTML } = e.currentTarget;
+    setClickedCategory(innerHTML);
   };
 
   return (
-    <>
-      {isLogin ? (
-        <Header
-          isLogin={isLogin}
-          clickedCategory={clickedCategory}
-          handleClickCategory={handleClickCategory}
-          nickname={'나나나나나난나나나나나'}
-        />
-      ) : (
-        <Header isLogin={isLogin} handleClickCategory={handleClickCategory} />
-      )}
-      {children}
-    </>
+    <PageLayoutContainer>
+      <Header
+        clickedCategory={clickedCategory}
+        handleClickCategory={handleClickCategory}
+      />
+      {clickedCategory === category && children}
+    </PageLayoutContainer>
   );
 };
 
 export default PageLayout;
+
+const PageLayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
