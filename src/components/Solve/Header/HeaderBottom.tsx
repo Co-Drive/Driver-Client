71e31cca0e0b16b2b1368f4ee6082cb9baccd830
seoAreum <1971236@hansung.ac.kr>
2024-07-25 04:35:18 +0900

    chore: 상수 분리

--- src/components/Solve/Header/HeaderBottom.tsx
+++ src/components/Solve/Header/HeaderBottom.tsx
@@ -1,38 +1,12 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { IcArrowBottomGray, IcArrowTopGray, IcLinkGray } from '../../../assets';
+import { TAG_PLATFORM_LISTS } from '../../../constants/\bSolve/SolveConts';
 import {
   ClickedListProps,
   HeaderBottomProps,
   UpdateQuestionInfoProps,
 } from '../../../types/Solve/solveTypes';
-
-const TAG_PLATFORM_LISTS = [
-  {
-    category: 'tags',
-    placeholder: '유형을 선택해주세요 (복수선택 가능)',
-    options: [
-      '해시',
-      '스택/큐',
-      '힙 (Heap)',
-      '정렬',
-      '완전탐색',
-      '탐욕법 (Greedy)',
-      '동적계획법 (Dynamic Programming)',
-      '깊이 우선탐색 (DFS)',
-      '너비 우선 탐색 (BFS)',
-      '이분탐색',
-      '그래프',
-      '트리',
-      '투포인터',
-    ],
-  },
-  {
-    category: 'platform',
-    placeholder: '플랫폼을 선택해주세요',
-    options: ['백준', '프로그래머스', 'SWEA', '리트코드', '해커링크', '기타'],
-  },
-];
 
 const HeaderBottom = ({
   questionInfo,
