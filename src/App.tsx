8d8dd61b350bfbfd7531577f2418039b07ba857f
Seung taek Oh <elqkddl@naver.com>
2024-07-25 14:40:31 +0900

    chore : App.tsx 비우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components';
-import GroupJoin from './page/GroupJoin';
 import Router from './Router';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
@@ -9,7 +8,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
-      <GroupJoin />
     </ThemeProvider>
   );
 }
