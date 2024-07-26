0d60b3942c3deb980c3b8d639caaf0707c598f6e
kimmoonju-102 <kimmun3549@naver.com>
2024-07-27 00:57:20 +0900

    feat: select 퍼블리싱

---
+++ src/components/Register/Select.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { IcArrowBottomGray, IcArrowTopGray } from '../../assets';
+import CommonHashTag from '../../common/CommonHashTag';
+
+const Select = () => {
+  return (
+    <SelectContainer>
+      <HiddenInput />
+      <SelectOptions>
+        <CommonHashTag />
+      </SelectOptions>
+      <ArrowToggle>
+        <IcArrowTopGray />
+        <IcArrowBottomGray />
+      </ArrowToggle>
+    </SelectContainer>
+  );
+};
+
+const SelectContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 29.6rem;
+  height: 4.8rem;
+  padding: 1.5rem;
+
+  border-radius: 0.8rem;
+  background-color: ${({ theme }) => theme.colors.gray700};
+  cursor: pointer;
+`;
+
+const HiddenInput = styled.input`
+  display: none;
+`;
+
+const SelectOptions = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  margin-top: 0;
+`;
+
+const ArrowToggle = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export default Select;
