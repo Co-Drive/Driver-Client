0794fea17fc1643ab8e3598387a8fdb7272bf4a0
Seung taek Oh <elqkddl@naver.com>
2024-07-26 21:43:20 +0900

    feat: 사용자가 입력한 비밀번호를 렌더링하기

--- src/page/GroupComplete.tsx
+++ src/page/GroupComplete.tsx
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 import { Thumbnail } from '../assets';
 import CommonButton from '../common/CommonButton';
 import PageLayout from '../components/PageLayout/PageLayout';
-const GroupComplete = ({ thumbnailUrl }: { thumbnailUrl: string }) => {
+
+interface GroupCompleteProps {
+  thumbnailUrl: string;
+  groupPassword: string;
+}
+const GroupComplete = ({ thumbnailUrl, groupPassword }: GroupCompleteProps) => {
   const navigate = useNavigate();
   const baseUrl = window.location.origin; // 생성한 그룹 페이지가 만들어지면 대체 될 예정
 
@@ -26,7 +31,7 @@ const GroupComplete = ({ thumbnailUrl }: { thumbnailUrl: string }) => {
       <Title>그룹 생성이 완료되었어요!</Title>
       <PasswordContainer>
         <PasswordText>
-          비밀번호 <Password>아자자화이팅</Password>
+          비밀번호 <Password>{groupPassword}</Password>
         </PasswordText>
       </PasswordContainer>
       <ThumbnailContainer>
@@ -57,7 +62,6 @@ const Title = styled.h1`
   margin-top: 7.4rem;
   margin-bottom: 2.6rem;
 
-  background-color: blue;
   ${({ theme }) => theme.fonts.title_bold_32};
   color: ${({ theme }) => theme.colors.codrive_green};
 `;
@@ -65,27 +69,25 @@ const Title = styled.h1`
 const PasswordContainer = styled.div`
   margin-bottom: 4rem;
 
-  background-color: purple;
+  /* background-color: purple; */
 `;
 
 const PasswordText = styled.p`
   ${({ theme }) => theme.fonts.title_bold_20};
+  color: ${({ theme }) => theme.colors.gray100};
 `;
 
 const Password = styled.span`
-  background-color: brown;
+  ${({ theme }) => theme.fonts.title_bold_20};
+  color: ${({ theme }) => theme.colors.codrive_purple};
 `;
 
 const ThumbnailContainer = styled.div`
   margin-bottom: 6.6rem;
-
-  /* background-color: aqua; */
 `;
 
 const ButtonContainer = styled.span`
   display: flex;
   gap: 1.8rem;
   justify-content: center;
-
-  /* background-color: cornflowerblue; */
 `;
