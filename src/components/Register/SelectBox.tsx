f02f6cc4b4d9ffc76dfbb48cbadaa3b74aa3bfc4
kimmoonju-102 <kimmun3549@naver.com>
2024-07-27 14:59:52 +0900

    fix: 불필요한 useCallback 수정

--- src/components/Register/SelectBox.tsx
+++ src/components/Register/SelectBox.tsx
@@ -9,19 +9,16 @@ const SelectBox = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   // 드롭다운 토글
-  const handleToggleDropdown = useCallback(
-    (e: React.MouseEvent<HTMLDivElement> | null) => {
-      if (e) e.stopPropagation();
-      setIsOpen((prev) => !prev);
-    },
-    []
-  );
+  const handleToggleDropdown = (e: React.MouseEvent<HTMLDivElement> | null) => {
+    if (e) e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
 
   // 선택된 태그 값 업데이트
-  const handleChangeTag = useCallback((value: string) => {
+  const handleChangeTag = (value: string) => {
     setSelectedTag(value);
     setInputValue(value);
-  }, []);
+  };
 
   // 옵션을 클릭했을 때 호출되는 함수
   // 선택한 옵션이 현재 선택된 태그와 다를 경우 태그를 변경하고 드롭다운을 닫음
