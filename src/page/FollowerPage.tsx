7bb9b45ddee5c9d2e2cf871c0495445c3b67e94e
seoAreum <1971236@hansung.ac.kr>
2024-07-29 19:13:26 +0900

    feat: 팔로워 정보 보여주는 부분 퍼블리싱

--- src/page/FollowerPage.tsx
+++ src/page/FollowerPage.tsx
@@ -1,12 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PageLayout from '../components/PageLayout/PageLayout';
 
 const DUMMY = {
-  profileImg: '',
+  profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
   nickname: '코딩하는 갱얼쥐',
   isFollowed: false,
-  introduce:
-    '나를 소개하는 문구를 적어주세요.아아아 최대 두줄까지만 적을 수 있게 하는거어떠나를 소개하는 문구를 적어주세요.아아아 최대 두줄까지만 적을 수 있게 하는거어떠',
+  introduce: '안녕하세요? 풀스택 개발자 코딩하는 갱얼쥐입니다.30자',
   github: 'https://github.com/Arooming',
   group: [
     {
@@ -102,9 +101,26 @@ const DUMMY = {
 };
 
 const FollowerPage = () => {
+  const { profileImg, nickname, isFollowed, introduce, github } = DUMMY;
   return (
     <PageLayout category="홈">
-      <FollowerPageContainer></FollowerPageContainer>
+      <FollowerPageContainer>
+        <FollowerContainer>
+          <Img src={profileImg} />
+          <InfoContainer>
+            <TopInfoContainer>
+              <Nickname>{nickname}</Nickname>
+              <FollowBtn type="button" $isFollowed={isFollowed}>
+                {isFollowed ? '팔로잉' : '팔로우'}
+              </FollowBtn>
+            </TopInfoContainer>
+            <Introduce>{introduce}</Introduce>
+            <GithubContainer>
+              <Github>{github}</Github>
+            </GithubContainer>
+          </InfoContainer>
+        </FollowerContainer>
+      </FollowerPageContainer>
     </PageLayout>
   );
 };
@@ -117,4 +133,80 @@ const FollowerPageContainer = styled.section`
   flex-direction: column;
 
   padding: 8.6rem 25.7rem 20rem;
+`;
+
+const FollowerContainer = styled.article`
+  display: flex;
+  gap: 4.3rem;
+  align-items: center;
+
+  min-width: 92.4rem;
+
+  width: 100%;
+  padding-bottom: 4.3rem;
+
+  border-bottom: 0.1rem solid ${({ theme }) => theme.colors.gray600};
+`;
+const Img = styled.img`
+  width: 12.8rem;
+  height: 12.8rem;
+
+  border-radius: 2rem;
+
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.article`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const TopInfoContainer = styled.div`
+  display: flex;
+  gap: 4rem;
+  align-items: center;
+
+  margin: 0.1rem 0 1.6rem;
+`;
+
+const Nickname = styled.p`
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.title_bold_24};
+`;
+
+const FollowBtn = styled.button<{ $isFollowed: boolean }>`
+  padding: 0.6rem 1.6rem;
+
+  border-radius: 0.6rem;
+
+  ${({ $isFollowed, theme }) =>
+    $isFollowed
+      ? css`
+          background-color: ${theme.colors.gray700};
+          color: ${theme.colors.gray300};
+        `
+      : css`
+          background-color: ${theme.colors.codrive_purple};
+          color: ${theme.colors.white};
+        `};
+  ${({ theme }) => theme.fonts.title_bold_16};
+`;
+
+const Introduce = styled.p`
+  color: ${({ theme }) => theme.colors.gray200};
+  ${({ theme }) => theme.fonts.title_medium_20};
+`;
+
+const GithubContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+
+  margin-top: 2rem;
+`;
+
+const Github = styled.p`
+  color: ${({ theme }) => theme.colors.gray200};
+  ${({ theme }) => theme.fonts.body_eng_regular_14};
 `;
