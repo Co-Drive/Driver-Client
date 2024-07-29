a81a53b2341453989fb91b1007f152fa23c71a76
seoAreum <1971236@hansung.ac.kr>
2024-07-30 02:05:16 +0900

    feat: 디자인 변경사항 반영

--- src/components/Solution/Header/SolutionHeaderTop.tsx
+++ src/components/Solution/Header/SolutionHeaderTop.tsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
-import { IcInformation, IcStarGray, IcStarGreen } from '../../../assets';
+import {
+  BtnHeart,
+  BtnShare,
+  IcInformation,
+  IcStarGray,
+  IcStarGreen,
+} from '../../../assets';
 
 export interface SolutionHeaderTopProps {
   title: string;
@@ -14,26 +20,40 @@ const SolutionHeaderTop = ({
 }: SolutionHeaderTopProps) => {
   return (
     <SolutionHeaderTopContainer>
-      <TitleContainer>
+      <Date>{`작성일자 | ${date}`}</Date>
+      <TopContainer>
         <Title>{title}</Title>
-        <Date>{`작성일자 | ${date}`}</Date>
-      </TitleContainer>
 
-      <LevelContainer>
-        <LevelDetailContainer>
-          <LvText>난이도</LvText>
-          <LvText>|</LvText>
-          <LvStarContainer>
-            {paintedStarArr.map((painted, idx) => {
-              return (
-                <li key={idx}>{painted ? <IcStarGreen /> : <IcStarGray />}</li>
-              );
-            })}
-          </LvStarContainer>
-        </LevelDetailContainer>
+        <BtnIcContainer>
+          <BtnHeart />
+          <BtnShare />
+        </BtnIcContainer>
+      </TopContainer>
 
-        <IcInformation />
-      </LevelContainer>
+      <BottomContainer>
+        <LevelContainer>
+          <LevelDetailContainer>
+            <LvText>난이도</LvText>
+            <LvText>|</LvText>
+            <LvStarContainer>
+              {paintedStarArr.map((painted, idx) => {
+                return (
+                  <li key={idx}>
+                    {painted ? <IcStarGreen /> : <IcStarGray />}
+                  </li>
+                );
+              })}
+            </LvStarContainer>
+          </LevelDetailContainer>
+
+          <IcInformation />
+        </LevelContainer>
+
+        <BtnContainer>
+          <RemoveBtn type="button">삭제하기</RemoveBtn>
+          <ModifyBtn type="button">수정하기</ModifyBtn>
+        </BtnContainer>
+      </BottomContainer>
     </SolutionHeaderTopContainer>
   );
 };
@@ -42,15 +62,28 @@ export default SolutionHeaderTop;
 
 const SolutionHeaderTopContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  flex-direction: column;
 
   width: 100%;
+  margin-bottom: 2.4rem;
+
+  border-bottom: 0.1rem solid ${({ theme }) => theme.colors.gray600};
 `;
 
-const TitleContainer = styled.div`
+const Date = styled.p`
+  margin-bottom: 3.4rem;
+
+  color: ${({ theme }) => theme.colors.gray200};
+  ${({ theme }) => theme.fonts.body_medium_14};
+`;
+
+const TopContainer = styled.div`
   display: flex;
-  gap: 2.7rem;
-  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
 `;
 
 const Title = styled.p`
@@ -58,18 +91,27 @@ const Title = styled.p`
   ${({ theme }) => theme.fonts.title_bold_24};
 `;
 
-const Date = styled.p`
-  color: ${({ theme }) => theme.colors.gray200};
-  ${({ theme }) => theme.fonts.body_medium_14};
+const BtnIcContainer = styled.div`
+  display: flex;
+  gap: 1.8rem;
+
+  margin-right: 1.2rem;
+`;
+
+const BottomContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  margin: 1.8rem 0 2.7rem 0.4rem;
 `;
 
 const LevelContainer = styled.div`
   display: flex;
-  gap: 7.4rem;
+  gap: 1.8rem;
   justify-content: center;
   align-items: center;
-
-  margin: 9.1rem 0 3rem;
 `;
 
 const LevelDetailContainer = styled.div`
@@ -89,4 +131,25 @@ const LvStarContainer = styled.ul`
   gap: 0.4rem;
   justify-content: center;
   align-items: center;
+`;
+
+const BtnContainer = styled.div`
+  display: flex;
+  gap: 1.8rem;
+`;
+
+const commonBtnStyle = styled.button`
+  padding: 1rem 2rem;
+
+  border-radius: 0.8rem;
+  color: ${({ theme }) => theme.colors.gray100};
+  ${({ theme }) => theme.fonts.title_bold_16};
+`;
+
+const RemoveBtn = styled(commonBtnStyle)`
+  background-color: ${({ theme }) => theme.colors.gray600};
+`;
+
+const ModifyBtn = styled(commonBtnStyle)`
+  background-color: ${({ theme }) => theme.colors.gray500};
 `;
