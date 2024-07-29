cb5b2f1cd6942cc4d60403ade5f642a15fca584a
seoAreum <1971236@hansung.ac.kr>
2024-07-29 15:26:58 +0900

    chore: 더미 데이터 내용 추가 및 컴포넌트 props 전달

--- src/page/FollowerPage.tsx
+++ src/page/FollowerPage.tsx
@@ -1,26 +1,55 @@
 import styled from 'styled-components';
+import ParticipatingGroup from '../components/\bFollwer/ParticipatingGroup';
 import PageLayout from '../components/PageLayout/PageLayout';
 
 const DUMMY = {
   profileImg: '',
   nickname: '코딩하는 갱얼쥐',
-  userIntroduce:
+  isFollowed: false,
+  introduce:
     '나를 소개하는 문구를 적어주세요.아아아 최대 두줄까지만 적을 수 있게 하는거어떠나를 소개하는 문구를 적어주세요.아아아 최대 두줄까지만 적을 수 있게 하는거어떠',
   github: 'https://github.com/Arooming',
   group: [
     {
+      id: 0,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
       title: '이제는 더이상 물러날 곳이 없는 스터디',
       tags: ['#swift', '#java', '#javascript'],
       introduce:
         '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
     },
     {
+      id: 1,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
       title: '이제는 더이상 물러날 곳이 없는 스터디',
       tags: ['#swift', '#java', '#javascript'],
       introduce:
         '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
     },
     {
+      id: 2,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+    {
+      id: 3,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+    {
+      id: 4,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
       title: '이제는 더이상 물러날 곳이 없는 스터디',
       tags: ['#swift', '#java', '#javascript'],
       introduce:
@@ -68,9 +97,20 @@ const DUMMY = {
 };
 
 const FollowerPage = () => {
+  const {
+    // profileImg,
+    // nickname,
+    // isFollowed,
+    // introduce,
+    // github,
+    group,
+    // recommend,
+  } = DUMMY;
   return (
     <PageLayout category="홈">
-      <FollowerPageContainer></FollowerPageContainer>
+      <FollowerPageContainer>
+        <ParticipatingGroup group={group} />
+      </FollowerPageContainer>
     </PageLayout>
   );
 };
