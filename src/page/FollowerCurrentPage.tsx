7c36ded541b83305c61c5227d8722d7215a6538c
seoAreum <1971236@hansung.ac.kr>
2024-07-30 11:11:31 +0900

    feat: 헤더 컴포넌트 분리

--- src/page/FollowerCurrentPage.tsx
+++ src/page/FollowerCurrentPage.tsx
@@ -1,16 +1,7 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { IcArrowBottomGray } from '../assets';
+import Header from '../components/Follower/CurrentSituation/Header';
 import PageLayout from '../components/PageLayout/PageLayout';
-
-const GROUPS = [
-  '이제는 더이상 물러날 곳이 없는 스터디1',
-  '이제는 더이상 물러날 곳이 없는 스터디2',
-  '이제는 더이상 물러날 곳이 없는 스터디3',
-  '이제는 더이상 물러날 곳이 없는 스터디4',
-];
-
-const SORTING = ['최신순', '|', '가나다순'];
 
 const FollowerCurrentPage = () => {
   const [filter, setFilter] = useState({
@@ -19,7 +10,7 @@ const FollowerCurrentPage = () => {
     sorting: '최신순',
   });
 
-  const { clickedGroup, isOptionOpen, sorting } = filter;
+  const { isOptionOpen } = filter;
 
   const handleClickInput = () => {
     [
@@ -51,55 +42,12 @@ const FollowerCurrentPage = () => {
   return (
     <PageLayout category="홈">
       <FollowerCurrentPageContainer>
-        <Header>
-          <TitleContainer>
-            <Title>팔로워 현황</Title>
-            <WeeklyBoard>7월 15일 - 21일 주간 보드</WeeklyBoard>
-          </TitleContainer>
-
-          <FilterContainer>
-            <SelectContainer>
-              <InputContainer onClick={handleClickInput}>
-                <Input
-                  readOnly={true}
-                  placeholder="그룹 별 보기"
-                  value={clickedGroup}
-                />
-                <IcArrowBottomGray />
-              </InputContainer>
-
-              <OptionContainer $hidden={!isOptionOpen}>
-                {GROUPS.map((group: string) => {
-                  return (
-                    <Option
-                      key={group}
-                      onClick={() => {
-                        handleClickOption(group);
-                      }}
-                      $isClickedList={clickedGroup === group}
-                    >
-                      {group}
-                    </Option>
-                  );
-                })}
-              </OptionContainer>
-            </SelectContainer>
-
-            <SortContainer>
-              {SORTING.map((standard) => {
-                return (
-                  <Sorting
-                    key={standard}
-                    onClick={(e) => standard !== '|' && handleClickSorting(e)}
-                    $isClicked={sorting === standard}
-                  >
-                    {standard}
-                  </Sorting>
-                );
-              })}
-            </SortContainer>
-          </FilterContainer>
-        </Header>
+        <Header
+          filter={filter}
+          handleClickInput={handleClickInput}
+          handleClickOption={handleClickOption}
+          handleClickSorting={handleClickSorting}
+        />
       </FollowerCurrentPageContainer>
     </PageLayout>
   );
@@ -113,123 +61,4 @@ const FollowerCurrentPageContainer = styled.section`
   flex-direction: column;
 
   padding: 6rem 41.45rem 11.5rem;
-`;
-
-const Header = styled.header`
-  display: flex;
-  gap: 4.4rem;
-  justify-content: center;
-  flex-direction: column;
-
-  width: 100%;
-`;
-
-const TitleContainer = styled.div`
-  display: flex;
-  gap: 1.3rem;
-  align-items: end;
-
-  width: 100%;
-`;
-
-const Title = styled.p`
-  color: ${({ theme }) => theme.colors.white};
-  ${({ theme }) => theme.fonts.title_bold_24};
-`;
-
-const WeeklyBoard = styled.p`
-  color: ${({ theme }) => theme.colors.gray300};
-  ${({ theme }) => theme.fonts.body_medium_16};
-`;
-
-const FilterContainer = styled.div`
-  display: flex;
-  gap: 28rem;
-  justify-content: space-between;
-  align-items: center;
-
-  width: 100%;
-`;
-
-const SelectContainer = styled.article`
-  display: flex;
-  gap: 0.6rem;
-  justify-content: center;
-  flex-direction: column;
-  flex-grow: 1;
-  position: relative;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  padding: 1.2rem 1.2rem 1.2rem 2rem;
-
-  border-radius: 0.8rem;
-  background-color: ${({ theme }) => theme.colors.gray700};
-`;
-
-const Input = styled.input`
-  width: 15.3rem;
-
-  outline: none;
-
-  border: none;
-  background-color: transparent;
-  ${({ theme }) => theme.fonts.body_ligth_16};
-  color: ${({ theme }) => theme.colors.white};
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.gray300};
-  }
-`;
-
-const OptionContainer = styled.ul<{ $hidden: boolean }>`
-  display: ${({ $hidden }) => ($hidden ? 'none' : 'block')};
-  position: absolute;
-  top: 5.8rem;
-  z-index: 1;
-
-  width: 100%;
-  padding: 0.8rem;
-
-  border-radius: 0.8rem;
-  background-color: ${({ theme }) => theme.colors.gray700};
-
-  text-align: center;
-  ${({ theme }) => theme.fonts.body_ligth_16};
-`;
-
-const Option = styled.li<{ $isClickedList: boolean }>`
-  width: 100%;
-  padding: 0.8rem;
-
-  border-radius: 0.4rem;
-  background-color: ${({ theme, $isClickedList }) =>
-    $isClickedList ? theme.colors.gray500 : theme.colors.gray700};
-  ${({ theme }) => theme.fonts.body_ligth_16};
-  color: ${({ theme }) => theme.colors.white};
-
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow-x: hidden;
-
-  &:hover {
-    border-radius: 0.8rem;
-    background-color: ${({ theme }) => theme.colors.gray700};
-  }
-`;
-
-const SortContainer = styled.div`
-  display: flex;
-  gap: 0.8rem;
-  align-items: center;
-`;
-
-const Sorting = styled.p<{ $isClicked: boolean }>`
-  color: ${({ $isClicked, theme }) =>
-    $isClicked ? theme.colors.white : theme.colors.gray500};
-  ${({ theme }) => theme.fonts.body_medium_14};
 `;
