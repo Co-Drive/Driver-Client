459809d47ad6169801665f0014176b79770caa25
5wintaek <elqkddl@naver.com>
2024-08-01 00:13:36 +0900

    Update src/components/GroupCreate/CreateButton.tsx
    Co-authored-by: seoAreum <80264647+Arooming@users.noreply.github.com>

--- src/components/GroupCreate/CreateButton.tsx
+++ src/components/GroupCreate/CreateButton.tsx
@@ -6,13 +6,8 @@ const CreateButton = () => {
   const [isActive, setIsActive] = useState(false);
 
   const handleCreateButtonClick = () => {
-    if (!isActive) {
       setIsActive(true);
-      console.log('그룹를 생성하도록 새로운 것입니다');
       // navigator 로 이동
-    } else {
-      console.log('에러발생');
-    }
   };
 
   return (
