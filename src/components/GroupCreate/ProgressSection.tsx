cc1c3f6fa879d00cbcbf3d6e96db4f0974942192
Seung taek Oh <elqkddl@naver.com>
2024-07-31 23:14:10 +0900

    feat: 공통TextArea 사용

--- src/components/GroupCreate/ProgressSection.tsx
+++ src/components/GroupCreate/ProgressSection.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import { PLACEHOLDER } from '../../constants/CommonTextarea/textareaConst';
+import CommonTextarea from '../../common/CommonTextarea';
 
 const ProgressSection = () => {
+  const [value, setValue] = useState('');
+
+  const handleChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+
+    setValue(value);
+  };
+
   return (
     <Section>
       <Label>
         진행 방식 <Essential>*</Essential>
       </Label>
-      <ProgressText maxLength={1000} placeholder={PLACEHOLDER[1]} />
+      <CommonTextarea
+        category="group"
+        value={value}
+        handleChangeTextarea={handleChangeTextarea}
+      />
     </Section>
   );
 };
@@ -16,22 +29,6 @@ export default ProgressSection;
 
 const Section = styled.section`
   margin-top: 4.8rem;
-`;
-
-const ProgressText = styled.textarea`
-  display: flex;
-  align-items: center;
-
-  width: 100%;
-  height: 26rem;
-  padding: 1.5rem 2rem 1.4rem;
-  resize: none;
-
-  border: none;
-  border-radius: 0.8rem;
-  ${({ theme }) => theme.fonts.body_ligth_16};
-  background-color: ${({ theme }) => theme.colors.gray700};
-  color: ${({ theme }) => theme.colors.gray300};
 `;
 
 const Label = styled.label`
