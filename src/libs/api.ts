0163aaf9b47ea472196f554b5c057cc84d6ec6d5
seoAreum <1971236@hansung.ac.kr>
2024-08-01 02:21:57 +0900

    feat: axios instance 초기 구현

---
+++ src/libs/api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
+});
+
+const token = sessionStorage.getItem('token');
+api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
