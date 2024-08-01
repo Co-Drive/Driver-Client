b07cafaa8e84878a8befa674fbf03f488bef8679
seoAreum <1971236@hansung.ac.kr>
2024-08-02 02:25:24 +0900

    feat: 서버통신으로 받아온 결과를 화면에 보여주도록 구현

--- src/page/SolutionPage.tsx
+++ src/page/SolutionPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 import { IcCode } from '../assets';
 import CodeEditor from '../common/CodeSpace/CodeEditor';
@@ -7,86 +7,93 @@ import Memo from '../common/CodeSpace/Memo';
 import PageLayout from '../components/PageLayout/PageLayout';
 import SolutionHeaderBottom from '../components/Solution/Header/SolutionHeaderBottom';
 import SolutionHeaderTop from '../components/Solution/Header/SolutionHeaderTop';
-
-const DUMMY = [
-  {
-    title: '전생했더니 슬라임 연구자가 아니었던 건에 대하여',
-    date: '2024.07.23',
-    level: 3,
-    tags: ['스택/큐', '동적계획법 (Dynamic Programming)'],
-    platform: '프로그래머스',
-    problemUrl: 'https://roseline124.github.io/djdefagrssd.dfdsdfds',
-    codeblocks: [
-      {
-        code: 'from django.contrib.auth import views as auth_views를 추가해야 Django auth에 내장되어 있는 LoginView, LogoutView를 사용할 수 있다. 따라서 앱 폴더의 views.py에서 따로 코드를 작성할 필요가 없다.\n\nas auth_views로 alias(별칭)을 주는 이유는 앱 폴더 내에 있는 views.py와 충돌하지 않도록 다른 이름을 사용한 것이다.',
-        memo: 'from django.contrib.auth import views as auth_views를 추가해야 Django auth에 내장되어 있는 LoginView, LogoutView를 사용할 수 있다. 따라서 앱 폴더의 views.py에서 따로 코드를 작성할 필요가 없다.\n\nas auth_views로 alias(별칭)을 주는 이유는 앱 폴더 내에 있는 views.py와 충돌하지 않도록 다른 이름을 사용한 것이다.',
-      },
-      {
-        code: '// 프로세스\nfunction solution(priorities, location) {\n  var cnt = 0;\n  const obj = priorities.map((prio, idx) => {\n    return { prio, idx };\n  });\n\n  while (obj.length) {\n    const MAX = Math.max(...priorities);\n\n    const process = obj.shift();\n    const prio = priorities.shift();\n    if (process.prio < MAX) {\n      obj.push(process);\n      priorities.push(prio);\n    } else {\n      cnt++;\n      if (process.idx === location) {\n        return cnt;\n      }\n    }\n  }\n\n  return cnt;\n}\n\nconsole.log(solution([2, 1, 3, 2], 2));\nconsole.log(solution([1, 1, 9, 1, 1, 1], 0));\n',
-        memo: '',
-      },
-    ],
-  },
-];
+import { getRecords } from '../libs/apis/Solution/getRecords';
 
 const SolutionPage = () => {
   const { state } = useLocation();
+  const { id } = useParams();
+  if (!id) return;
+
+  const [records, setRecords] = useState<{
+    title: string;
+    // date: string;
+    level: number;
+    tags: Array<string>;
+    platform: string;
+    problemUrl: string;
+    codeblocks: Array<{
+      code: string;
+      memo: string;
+    }>;
+  }>();
+
+  const fetchData = async () => {
+    try {
+      const { data } = await getRecords(parseInt(id));
+      const { title, level, tags, platform, problemUrl, codeblocks } = data;
+      setRecords({
+        title,
+        level,
+        tags,
+        platform,
+        problemUrl,
+        codeblocks,
+      });
+    } catch (err) {
+      // 추후 삭제, navigate 코드로 대체할 예정
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [id]);
 
   return (
     <PageLayout category="문제풀이">
-      <SolutionPageContainer>
-        {DUMMY.map((data) => {
-          const { title, date, level, tags, platform, problemUrl, codeblocks } =
-            data;
-          const paintedStarArr: Array<number> = Array(level)
-            .fill(1)
-            .concat(Array(5 - level).fill(0));
+      {records ? (
+        <SolutionPageContainer>
+          <SolutionPageHeader>
+            <SolutionHeaderTop
+              followerInfo={state}
+              title={records.title}
+              date={'2024.07.23'}
+              paintedStarArr={Array(records.level)
+                .fill(1)
+                .concat(Array(5 - records.level).fill(0))}
+            />
 
-          return (
-            <React.Fragment key={title}>
-              <SolutionPageHeader>
-                <SolutionHeaderTop
-                  followerInfo={state}
-                  title={title}
-                  date={date}
-                  paintedStarArr={paintedStarArr}
+            <SolutionHeaderBottom
+              tags={records.tags}
+              platform={records.platform}
+              problemUrl={records.problemUrl}
+            />
+          </SolutionPageHeader>
+
+          {records.codeblocks.map((codeblock, idx) => {
+            const { code, memo } = codeblock;
+            return (
+              <CodeBlckContainer key={idx} $isFirstCodeBlock={idx === 0}>
+                <TopBar>
+                  <TextContainer>
+                    <IcCode />
+                    <Text>codeblock</Text>
+                  </TextContainer>
+                </TopBar>
+
+                <CodeEditor
+                  isReadOnly={true}
+                  stringId={idx.toString()}
+                  code={code}
                 />
-
-                <SolutionHeaderBottom
-                  tags={tags}
-                  platform={platform}
-                  problemUrl={problemUrl}
-                />
-              </SolutionPageHeader>
-
-              {codeblocks.map((codeblock, idx) => {
-                const { code, memo } = codeblock;
-                return (
-                  <CodeBlckContainer key={idx} $isFirstCodeBlock={idx === 0}>
-                    <TopBar>
-                      <TextContainer>
-                        <IcCode />
-                        <Text>codeblock</Text>
-                      </TextContainer>
-                    </TopBar>
-
-                    <CodeEditor
-                      isReadOnly={true}
-                      stringId={idx.toString()}
-                      code={code}
-                    />
-                    <Memo
-                      isReadOnly={true}
-                      stringId={idx.toString()}
-                      memo={memo}
-                    />
-                  </CodeBlckContainer>
-                );
-              })}
-            </React.Fragment>
-          );
-        })}
-      </SolutionPageContainer>
+                <Memo isReadOnly={true} stringId={idx.toString()} memo={memo} />
+              </CodeBlckContainer>
+            );
+          })}
+        </SolutionPageContainer>
+      ) : (
+        <div>Loading...</div>
+      )}
     </PageLayout>
   );
 };
