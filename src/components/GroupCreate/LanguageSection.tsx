e89dcb231526c83b7ccdfa7f13e88cac32040cc8
Seung taek Oh <elqkddl@naver.com>
2024-08-02 01:25:18 +0900

    feat: 태그 및 드롭박스 다운

--- src/components/GroupCreate/LanguageSection.tsx
+++ src/components/GroupCreate/LanguageSection.tsx
@@ -1,17 +1,83 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { IcArrowBottomGray } from '../../assets';
+import CommonHashTag from '../../common/CommonHashTag';
+const DUMMY = [
+  'Python',
+  'Java',
+  'C',
+  'C++',
+  'C#',
+  'Javascript',
+  'Kotlin',
+  'Ruby',
+  'Swift',
+  'Scala',
+  'Go',
+];
+
+interface LanguageSectionProps {
+  removeTag: (tag: string) => void;
+}
+
 const LanguageSection = () => {
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    console.log('토글다운');
+
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  const addTag = (tag: string) => {
+    if (selectedTags.length < 5 && !selectedTags.includes(tag)) {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const removeTag = (tag: string) => {
+    setSelectedTags(selectedTags.filter((t) => t !== tag));
+  };
+
   return (
     <Section>
       <Label>
         사용 언어 <Essential>*</Essential>
       </Label>
-      <DropdownHeader>
-        <Dropdown>복수선택 가능</Dropdown>
-        <IconContainer>
-          <IcArrowBottomGray />
-        </IconContainer>
-      </DropdownHeader>
+      <div>
+        <DropdownContainer onClick={toggleDropdown}>
+          <div>
+            {selectedTags.length === 0 ? (
+              <DropdownText>최대 5개까지 선택해 주세요</DropdownText>
+            ) : (
+              selectedTags.map((tag, index) => (
+                <CommonHashTag
+                  key={index}
+                  selectedTag={tag}
+                  removeTag={() => removeTag(tag)}
+                />
+              ))
+            )}
+          </div>
+          <IconContainer>
+            <IcArrowBottomGray />
+          </IconContainer>
+        </DropdownContainer>
+      </div>
+      {isDropdownOpen && (
+        <DropdownItemContainer>
+          {DUMMY.map((tag) => (
+            <DropdownItem
+              key={tag}
+              $isSelected={selectedTags.includes(tag)}
+              onClick={() => addTag(tag)}
+            >
+              {tag}
+            </DropdownItem>
+          ))}
+        </DropdownItemContainer>
+      )}
     </Section>
   );
 };
@@ -24,7 +90,7 @@ const Section = styled.section`
   margin-top: 4rem;
 `;
 
-const DropdownHeader = styled.div`
+const DropdownContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -34,18 +100,58 @@ const DropdownHeader = styled.div`
 
   border-radius: 0.8rem;
   background-color: ${({ theme }) => theme.colors.gray700};
+
+  /* background-color: pink; */
   cursor: pointer;
 `;
 
-const Dropdown = styled.div`
-  padding: 1.5rem 2rem 1.4rem;
+const DropdownHeader = styled.div`
+  /* background-color: pink; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   color: ${({ theme }) => theme.colors.gray300};
   ${({ theme }) => theme.fonts.body_ligth_16};
 `;
 
+const DropdownItemContainer = styled.div`
+  display: flex;
+
+  padding: 1.6rem;
+
+  border-radius: 0.8rem;
+  background-color: ${({ theme }) => theme.colors.gray700};
+  cursor: pointer;
+`;
+
+const DropdownItem = styled.div<{ $isSelected: boolean }>`
+  display: inline-flex;
+  align-items: center;
+
+  padding: 0.6rem 1rem;
+  margin-right: 1rem;
+
+  border-radius: 0.4rem;
+  ${({ theme }) => theme.fonts.body_eng_medium_12};
+  background-color: ${({ theme }) => theme.colors.codrive_green};
+  color: ${({ theme }) => theme.colors.bg};
+`;
+
 const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+
   margin-right: 1.2rem;
+
+  /* background-color: blue; */
+`;
+
+const DropdownText = styled.p`
+  padding: 1.5rem 2rem 1.4rem;
+
+  ${({ theme }) => theme.fonts.body_ligth_16};
+  color: ${({ theme }) => theme.colors.gray300};
 `;
 
 const Label = styled.label`
