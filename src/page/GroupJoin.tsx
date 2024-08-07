80336c55a1756f43c12efe4b53413d126147d308
Seung taek Oh <elqkddl@naver.com>
2024-08-07 15:58:18 +0900

    feat: API 통신 실패 - 1

--- src/page/GroupJoin.tsx
+++ src/page/GroupJoin.tsx
@@ -4,27 +4,26 @@ import { IcSecretBigWhite } from '../assets';
 import CommonButton from '../common/CommonButton';
 import CommonInput from '../common/CommonInput';
 import PageLayout from '../components/PageLayout/PageLayout';
+import { postAnswer } from '../libs/apis/GroupCreate/postAnswer';
 
 const GroupJoin = () => {
   const [password, setPassword] = useState('');
   const [isActive, setIsActive] = useState(false);
   const [isNotMatchedPW, setIsNotMatchedPW] = useState(false);
-  const correctPassword = false; // 백엔드 사용자PW 비밀번호
+  // const correctPassword = false; // 백엔드 사용자PW 비밀번호
 
   const handleChangeInputs = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setPassword(value);
     setIsActive(value.length > 0);
     setIsNotMatchedPW(false); // 비밀번호가 틀렸다고 표시된 상태 초기화
+    console.log('패스워드 input');
   };
 
-  const handleButtonClick = () => {
-    if (correctPassword) {
-      console.log('그룹에 성공적으로 참여하였습니다');
-    } else {
-      setIsNotMatchedPW(true);
-      console.log('비밀번호가 틀렸습니다');
-    }
+  const handleButtonClick = async () => {
+    console.log('현재 패스워드 값 :', password); // 디버깅: 버튼 클릭 시의 비밀번호 값 확인
+    const data = await postAnswer(password);
+    console.log(data);
   };
 
   return (
