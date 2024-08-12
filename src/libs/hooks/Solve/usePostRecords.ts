27c079022037fa10e743053dfc8aec938e75272b
seoAreum <1971236@hansung.ac.kr>
2024-08-12 19:32:53 +0900

    feat: 변경된 내용이 바로 반영되도록 invalidateQueries 추가

--- src/libs/hooks/Solve/usePostRecords.ts
+++ src/libs/hooks/Solve/usePostRecords.ts
@@ -1,10 +1,12 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { PostRecordsProps } from '../../../types/Solve/solveTypes';
 import { postRecords } from '../../apis/Solve/postRecords';
 
 const usePostRecords = () => {
   const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async ({ questionInfo, codeblocks }: PostRecordsProps) => {
       return await postRecords({ questionInfo, codeblocks });
@@ -12,6 +14,7 @@ const usePostRecords = () => {
     onError: () => navigate('/error'),
     onSuccess: ({ data }) => {
       const { recordId } = data;
+      queryClient.invalidateQueries({ queryKey: ['get-monthly-solution'] });
       navigate(`/solution/${recordId}`);
     },
   });
