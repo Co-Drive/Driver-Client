1a3a2d5a26efbad7af0c587c8ec3c3dd984e124f
seoAreum <1971236@hansung.ac.kr>
2024-08-12 21:56:24 +0900

    feat: 변경된 임시저장 데이터가 바로 반영되도록 invalidateQueries 추가

--- src/libs/hooks/Solve/usePostTempRecords.ts
+++ src/libs/hooks/Solve/usePostTempRecords.ts
@@ -1,10 +1,12 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { postTempRecords } from '../../apis/Solve/postTempRecords';
 import { PostRecordsProps } from './../../../types/Solve/solveTypes';
 
 const usePostTempRecords = (onClose?: () => void) => {
   const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async ({ questionInfo, codeblocks }: PostRecordsProps) => {
       return await postTempRecords({ questionInfo, codeblocks });
@@ -12,6 +14,7 @@ const usePostTempRecords = (onClose?: () => void) => {
     onError: () => navigate('/error'),
     onSuccess: ({ data }) => {
       if (data) {
+        queryClient.invalidateQueries({ queryKey: ['/get-temp-records'] });
         navigate('/solution');
         onClose && onClose();
       }
