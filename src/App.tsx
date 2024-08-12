e8fbefb0076715e05b1414c1c305182fb172bad1
seoAreum <1971236@hansung.ac.kr>
2024-08-12 17:06:01 +0900

    feat: 리액트 쿼리 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
 
 function App() {
+  const [queryClient] = useState(new QueryClient());
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
