8371e7d87aee12a97322d61dd93233740872eda9
Seung taek Oh <elqkddl@naver.com>
2024-08-13 18:24:25 +0900

    feat: uuid 안에 들어있는 비밀번호 및 이미지주소 뿌려주기

--- src/page/GroupComplete.tsx
+++ src/page/GroupComplete.tsx
@@ -1,21 +1,46 @@
-import { useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import CommonButton from '../common/CommonButton';
 import Modal from '../common/Modal/Modal';
 import PageLayout from '../components/PageLayout/PageLayout';
+import { api } from '../libs/api';
 import { handleCopyClipBoard } from '../utils/handleCopyClipBoard';
 
 const GroupComplete = () => {
   const navigate = useNavigate();
-  const { state } = useLocation();
-  const { groupPassword, thumbnailUrl } = state || {};
-  const baseUrl = window.location.origin; // 생성한 그룹 페이지가 만들어지면 대체 될 예정
+  const [groupPassword, setGroupPassword] = useState('');
+  const [thumbnailUrl, setThumbnailUrl] = useState('');
   const [isCopied, setIsCopied] = useState(false);
-  const { id } = useParams();
-  console.log(id);
+  const { id: uuid } = useParams();
+  const token = sessionStorage.getItem('token');
+  const nickname = sessionStorage.getItem('nickname');
+
+  useEffect(() => {
+    if (token && nickname) {
+      const fetchGroupInfo = async () => {
+        try {
+          const response = await api.get(`rooms/uuid/${uuid}`);
+          const { data } = response.data;
+          // console.log(data.password);
+
+          setGroupPassword(data.password);
+          setThumbnailUrl(data.imageSrc);
+
+          console.log('비밀번호 :', data.password);
+          console.log('이미지 : ', data.imageSrc);
+        } catch (error) {
+          console.log('error');
+        }
+      };
+      fetchGroupInfo();
+    } else {
+      navigate('/login');
+    }
+  }, [uuid, token, nickname]);
 
   const handleClickCopyBtn = () => {
+    const baseUrl = window.location.origin; // 생성한 그룹 페이지가 만들어지면 대체 될 예정
     handleCopyClipBoard({ baseUrl: baseUrl, isUsedBaseUrl: true });
     setIsCopied(true);
     setTimeout(() => {
