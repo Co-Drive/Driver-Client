c1969eeabf5df5c48ee0949fd3d10a76c1de6ff0
seoAreum <1971236@hansung.ac.kr>
2024-08-15 20:46:30 +0900

    feat: 그룹 카테고리 선택 컴포넌트 퍼블리싱

--- src/components/MyGroup/RecommendGroup.tsx
+++ src/components/MyGroup/RecommendGroup.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import RecommendCard from '../../common/RecommendCard';
+import { SORTING } from '../../constants/Follower/currentConst';
 import { RecommendGroupProps } from '../../types/MyGroup/myGroupType';
 
 const RecommendGroup = ({ user, group }: RecommendGroupProps) => {
-  const [isClicked, setIsClicked] = useState('내가 참여한 그룹');
+  const GROUP_CATEGORY = ['내가 참여한 그룹', '내가 생성한 그룹'];
+
+  const [isClicked, setIsClicked] = useState(GROUP_CATEGORY[0]);
+  const [sorting, setSorting] = useState('최신순');
 
   const handleClickCategory = (
     e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
@@ -13,22 +17,44 @@ const RecommendGroup = ({ user, group }: RecommendGroupProps) => {
     setIsClicked(innerHTML);
   };
 
+  const handleClickSorting = (
+    e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
+  ) => {
+    const { innerHTML } = e.currentTarget;
+    setSorting(innerHTML);
+    // 최신순/ 가나다순에 따라 서버 통신 들어갈 예정
+  };
+
   return (
     <Recommendcontainer>
       <RecommendHeader>
-        <Category
-          onClick={handleClickCategory}
-          $isClickedCategory={isClicked === '내가 참여한 그룹'}
-        >
-          내가 참여한 그룹
-        </Category>
-        <Category
-          onClick={handleClickCategory}
-          $isClickedCategory={isClicked === '내가 생성한 그룹'}
-        >
-          내가 생성한 그룹
-        </Category>
+        {GROUP_CATEGORY.map((category, idx) => {
+          return (
+            <Category
+              key={idx}
+              onClick={handleClickCategory}
+              $isClickedCategory={isClicked === category}
+            >
+              {category}
+            </Category>
+          );
+        })}
       </RecommendHeader>
+
+      <SortContainer>
+        {SORTING.map((standard) => {
+          return (
+            <Sorting
+              key={standard}
+              onClick={(e) => standard !== '|' && handleClickSorting(e)}
+              $isClicked={sorting === standard}
+            >
+              {standard}
+            </Sorting>
+          );
+        })}
+      </SortContainer>
+
       <RecommendCard user={user} group={group} />
     </Recommendcontainer>
   );
@@ -38,8 +64,9 @@ export default RecommendGroup;
 
 const Recommendcontainer = styled.article`
   display: flex;
-  gap: 3rem;
+  gap: 1.6rem;
   justify-content: center;
+  align-items: end;
   flex-direction: column;
 
   width: 100%;
@@ -70,4 +97,18 @@ const Category = styled.p<{ $isClickedCategory: boolean }>`
         `};
 
   ${({ theme }) => theme.fonts.title_medium_24};
+`;
+
+const SortContainer = styled.div`
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+
+  margin-top: 0.2rem;
+`;
+
+const Sorting = styled.p<{ $isClicked: boolean }>`
+  color: ${({ $isClicked, theme }) =>
+    $isClicked ? theme.colors.white : theme.colors.gray500};
+  ${({ theme }) => theme.fonts.body_medium_14};
 `;
