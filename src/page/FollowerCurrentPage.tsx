6c0e81b5e0457934e12db786133e5d577e592b54
seoAreum <1971236@hansung.ac.kr>
2024-08-15 17:36:59 +0900

    feat: 디자인 변경에 따라 렌더링 컴포넌트 변경

--- src/page/FollowerCurrentPage.tsx
+++ src/page/FollowerCurrentPage.tsx
@@ -1,98 +1,18 @@
-import { useState } from 'react';
-import styled, { css } from 'styled-components';
-import { IcArrowLeftSmallGray, IcArrowRightSmallGray } from '../assets';
-import Header from '../components/Follower/CurrentSituation/Header';
-import WeeklyCurrent from '../components/Follower/CurrentSituation/WeeklyCurrent';
+import styled from 'styled-components';
+import FollowerList from '../components/Follower/Current/FollowerList';
 import PageLayout from '../components/PageLayout/PageLayout';
-import { TOTAL_PAGES } from '../constants/Follower/currentConst';
 
 const FollowerCurrentPage = () => {
-  const [filter, setFilter] = useState({
-    clickedGroup: '',
-    isOptionOpen: false,
-    sorting: '최신순',
-  });
-
-  const [clickedPage, setClickedPage] = useState(1);
-
-  // 첫 화면에서 서버에 전체 리스트 수 아니면 전체 페이지 수 요청
-  const pagesArr = Array(TOTAL_PAGES).fill(0);
-
-  const { isOptionOpen } = filter;
-
-  const handleClickInput = () => {
-    [
-      setFilter({
-        ...filter,
-        isOptionOpen: !isOptionOpen,
-      }),
-    ];
-  };
-
-  const handleClickOption = (selectedGroup: string) => {
-    setFilter({
-      ...filter,
-      clickedGroup: selectedGroup,
-      isOptionOpen: false,
-    });
-    // 선택한 옵션에 따라 서버 통신 들어갈 예정
-  };
-
-  const handleClickSorting = (
-    e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
-  ) => {
-    const { innerHTML } = e.currentTarget;
-    setFilter({
-      ...filter,
-      sorting: innerHTML,
-    });
-    // 최신순/ 가나다순에 따라 서버 통신 들어갈 예정
-  };
-
-  const handleClickPrevBtn = () => {
-    setClickedPage((prev) => prev - 1);
-  };
-
-  const handleClickPageNumber = (page: number) => {
-    setClickedPage(page);
-  };
-
-  const handleClickNextBtn = () => {
-    setClickedPage((prev) => prev + 1);
-  };
-
   return (
     <PageLayout category="홈">
       <FollowerCurrentPageContainer>
-        <Header
-          filter={filter}
-          handleClickInput={handleClickInput}
-          handleClickOption={handleClickOption}
-          handleClickSorting={handleClickSorting}
-        />
+        <Header>
+          <Title>팔로워 현황</Title>
+          <Date>7월 15일 - 21일</Date>
+          <Text>주간보드</Text>
+        </Header>
 
-        <WeeklyCurrent clickedPage={clickedPage} />
-
-        <PageNationBar>
-          <IcArrowLeftSmallGray
-            onClick={() => clickedPage !== 1 && handleClickPrevBtn()}
-          />
-          {pagesArr.map((_, idx) => {
-            const page = idx + 1;
-            return (
-              <PageNumber
-                key={page}
-                $isClicked={clickedPage === page}
-                onClick={() => handleClickPageNumber(page)}
-              >
-                {page}
-              </PageNumber>
-            );
-          })}
-          <IcArrowRightSmallGray
-            onClick={() => clickedPage !== TOTAL_PAGES && handleClickNextBtn()}
-          />
-        </PageNationBar>
+        <FollowerList />
       </FollowerCurrentPageContainer>
     </PageLayout>
   );
@@ -105,35 +25,30 @@ const FollowerCurrentPageContainer = styled.section`
   align-items: center;
   flex-direction: column;
 
-  min-width: 61.1rem;
-
   width: 100%;
-  padding: 6rem 41.45rem 11.5rem;
+  padding: 6rem 25.7rem 29.4rem;
 `;
 
-const PageNationBar = styled.div`
+const Header = styled.header`
   display: flex;
-  gap: 1.2rem;
-  justify-content: center;
-  align-items: center;
+  gap: 1.3rem;
+  align-items: end;
 
   width: 100%;
-  margin-top: 4.8rem;
 `;
 
-const PageNumber = styled.p<{ $isClicked: boolean }>`
-  padding: 0.4rem 1rem;
+const Title = styled.p`
+  margin-bottom: -0.3rem;
 
-  border-radius: 0.4rem;
-  ${({ theme, $isClicked }) =>
-    $isClicked
-      ? css`
-          background-color: ${theme.colors.gray700};
-          color: ${theme.colors.white};
-        `
-      : css`
-          background-color: transparent;
-          color: ${theme.colors.gray200};
-        `};
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.title_bold_24};
+`;
+
+const Date = styled.p`
+  color: ${({ theme }) => theme.colors.gray300};
   ${({ theme }) => theme.fonts.body_medium_16};
+`;
+
+const Text = styled(Date)`
+  margin-left: 0.4rem;
 `;
