b6e6873b27c42717c35b7caff78a0400fda7f1d3
seoAreum <1971236@hansung.ac.kr>
2024-08-15 17:37:25 +0900

    chore: 데이터 추가 및 삭제

--- src/constants/Follower/currentConst.ts
+++ src/constants/Follower/currentConst.ts
@@ -6,232 +6,82 @@ export const DUMMY = {
   followers: [
     {
       id: 1,
-      profile: {
-        imgSrc: 'https://avatars.githubusercontent.com/u/80264647?v=4',
-        nickname: '닉네임은총열글자입니',
-      },
-      boards: [
-        {
-          date: '26',
-          count: 1,
-        },
-        {
-          date: '27',
-          count: 1,
-        },
-        {
-          date: '28',
-          count: 1,
-        },
-        {
-          date: '29',
-          count: 1,
-        },
-        {
-          date: '30',
-          count: 1,
-        },
-        {
-          date: '31',
-          count: 1,
-        },
-        {
-          date: '1',
-          count: 1,
-        },
-      ],
+      imgSrc: 'https://avatars.githubusercontent.com/u/80264647?v=4',
+      nickname: '닉네임은총열글자입니',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
     },
     {
       id: 2,
-      profile: {
-        imgSrc:
-          'https://i.pinimg.com/236x/b7/cd/1a/b7cd1acf6dc183795741c1c5ead481fd.jpg',
-        nickname: '2번사용자어쩌구저쩌',
-      },
-      boards: [
-        {
-          date: '26',
-          count: 1,
-        },
-        {
-          date: '27',
-          count: 1,
-        },
-        {
-          date: '28',
-          count: 1,
-        },
-        {
-          date: '29',
-          count: 1,
-        },
-        {
-          date: '30',
-          count: 1,
-        },
-        {
-          date: '31',
-          count: 1,
-        },
-        {
-          date: '1',
-          count: 1,
-        },
-      ],
+      imgSrc:
+        'https://i.pinimg.com/236x/b7/cd/1a/b7cd1acf6dc183795741c1c5ead481fd.jpg',
+      nickname: '2번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
     },
     {
       id: 3,
-      profile: {
-        imgSrc:
-          'https://i.pinimg.com/236x/b8/03/af/b803afd5c604e555159e683072e4ca31.jpg',
-        nickname: '3번매링매링',
-      },
-      boards: [
-        {
-          date: '26',
-          count: 1,
-        },
-        {
-          date: '27',
-          count: 1,
-        },
-        {
-          date: '28',
-          count: 1,
-        },
-        {
-          date: '29',
-          count: 1,
-        },
-        {
-          date: '30',
-          count: 1,
-        },
-        {
-          date: '31',
-          count: 1,
-        },
-        {
-          date: '1',
-          count: 1,
-        },
-      ],
+      imgSrc:
+        'https://i.pinimg.com/236x/b8/03/af/b803afd5c604e555159e683072e4ca31.jpg',
+      nickname: '3번매링매링',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
     },
     {
       id: 4,
-      profile: {
-        imgSrc:
-          'https://i.pinimg.com/236x/df/13/e6/df13e6bfcd025ef8bc865b14b530f300.jpg',
-        nickname: '4번사용자어쩌구저쩌',
-      },
-      boards: [
-        {
-          date: '26',
-          count: 1,
-        },
-        {
-          date: '27',
-          count: 1,
-        },
-        {
-          date: '28',
-          count: 1,
-        },
-        {
-          date: '29',
-          count: 1,
-        },
-        {
-          date: '30',
-          count: 1,
-        },
-        {
-          date: '31',
-          count: 1,
-        },
-        {
-          date: '1',
-          count: 1,
-        },
-      ],
+      imgSrc:
+        'https://i.pinimg.com/236x/df/13/e6/df13e6bfcd025ef8bc865b14b530f300.jpg',
+      nickname: '4번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
     },
-
     {
       id: 5,
-      profile: {
-        imgSrc:
-          'https://i.pinimg.com/236x/a7/6a/47/a76a47686480dcfdd9a19043dbd99171.jpg',
-        nickname: '5번사용자',
-      },
-      boards: [
-        {
-          date: '26',
-          count: 1,
-        },
-        {
-          date: '27',
-          count: 1,
-        },
-        {
-          date: '28',
-          count: 1,
-        },
-        {
-          date: '29',
-          count: 1,
-        },
-        {
-          date: '30',
-          count: 1,
-        },
-        {
-          date: '31',
-          count: 1,
-        },
-        {
-          date: '1',
-          count: 1,
-        },
-      ],
+      imgSrc:
+        'https://i.pinimg.com/236x/a7/6a/47/a76a47686480dcfdd9a19043dbd99171.jpg',
+      nickname: '5번사용자',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
     },
-
     {
       id: 6,
-      profile: {
-        imgSrc:
-          'https://i.pinimg.com/564x/35/ea/46/35ea46d7ff554bdd4f47f52b7f535f5e.jpg',
-        nickname: '6번사용자어쩌구저쩌',
-      },
-      boards: [
-        {
-          date: '26',
-          count: 1,
-        },
-        {
-          date: '27',
-          count: 1,
-        },
-        {
-          date: '28',
-          count: 1,
-        },
-        {
-          date: '29',
-          count: 1,
-        },
-        {
-          date: '30',
-          count: 1,
-        },
-        {
-          date: '31',
-          count: 1,
-        },
-        {
-          date: '1',
-          count: 1,
-        },
-      ],
+      imgSrc:
+        'https://i.pinimg.com/564x/35/ea/46/35ea46d7ff554bdd4f47f52b7f535f5e.jpg',
+      nickname: '6번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
+    },
+    {
+      id: 7,
+      imgSrc:
+        'https://i.pinimg.com/564x/35/ea/46/35ea46d7ff554bdd4f47f52b7f535f5e.jpg',
+      nickname: '6번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
+    },
+    {
+      id: 8,
+      imgSrc:
+        'https://i.pinimg.com/564x/35/ea/46/35ea46d7ff554bdd4f47f52b7f535f5e.jpg',
+      nickname: '6번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
+    },
+    {
+      id: 9,
+      imgSrc:
+        'https://i.pinimg.com/564x/35/ea/46/35ea46d7ff554bdd4f47f52b7f535f5e.jpg',
+      nickname: '6번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
+    },
+    {
+      id: 10,
+      imgSrc:
+        'https://i.pinimg.com/564x/35/ea/46/35ea46d7ff554bdd4f47f52b7f535f5e.jpg',
+      nickname: '6번사용자어쩌구저쩌',
+      rate: 15,
+      problem: '[PCCP 기출문제] 2번 / 석유 시추',
     },
   ],
 };
