54235f53a4e18fbdeb5081efdc46fac0ada85f6c
seoAreum <1971236@hansung.ac.kr>
2024-08-21 18:49:36 +0900

    feat: 문제 푼 팔로워 페이지 변경 기능 구현

--- src/components/Follower/Current/TodaysSolver.tsx
+++ src/components/Follower/Current/TodaysSolver.tsx
@@ -1,17 +1,40 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import { IcInformation } from '../../../assets';
+import { IcArrowLeftSmallGray, IcArrowRightSmallGray } from '../../../assets';
+import { TODAYS_SOLVER_DUMMY } from '../../../constants/Follower/currentConst';
 import Solver from './Solver';
 
 const TodaysSolver = () => {
+  const { totalPages, users } = TODAYS_SOLVER_DUMMY;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleClickArrowLeft = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => (prev -= 1));
+    }
+  };
+
+  const handleClickArrowRight = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => (prev += 1));
+    }
+  };
+
   return (
     <TodaysSolverContainer>
       <Header>
         <Title>오늘 문제 푼 팔로워</Title>
-        <IcInformation />
+
+        {users.length && (
+          <ArrowContainer>
+            <IcArrowLeftSmallGray onClick={handleClickArrowLeft} />
+            <IcArrowRightSmallGray onClick={handleClickArrowRight} />
+          </ArrowContainer>
+        )}
       </Header>
 
       <SolverContainer>
-        <Solver />
+        <Solver currentPage={currentPage} users={users} />
       </SolverContainer>
     </TodaysSolverContainer>
   );
@@ -30,14 +53,23 @@ const TodaysSolverContainer = styled.article`
 const Header = styled.header`
   display: flex;
   gap: 2.4rem;
+  justify-content: space-between;
   align-items: center;
 
+  margin-right: 1rem;
   margin-left: 0.6rem;
 `;
 
 const Title = styled.p`
   color: ${({ theme }) => theme.colors.white};
   ${({ theme }) => theme.fonts.title_bold_20};
+`;
+
+const ArrowContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
 `;
 
 const SolverContainer = styled.article`
