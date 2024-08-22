b8426cbe1e8fb7131bf6e9f4266f9a3dd19a5611
seoAreum <1971236@hansung.ac.kr>
2024-08-22 15:58:46 +0900

    feat: 푼 문제 수에 따라 색상 다르게 배치

--- src/components/Follower/Current/FollowerCurrentGraph.tsx
+++ src/components/Follower/Current/FollowerCurrentGraph.tsx
@@ -1,4 +1,4 @@
-import { Bar, BarChart, ResponsiveContainer } from 'recharts';
+import { Bar, BarChart, Cell, ResponsiveContainer } from 'recharts';
 
 interface FollowerCurrentGraphProps {
   users: Array<{
@@ -11,18 +11,47 @@ interface FollowerCurrentGraphProps {
 const FollowerCurrentGraph = ({ users }: FollowerCurrentGraphProps) => {
   const data = users.map((user) => {
     const { nickname, problemNum } = user;
+    const height = problemNum * 10 + 10;
 
     return {
       name: nickname,
       problemNum: problemNum,
+      height: height,
     };
   });
 
   const filledData = Array(15)
     .fill(0)
     .map((_, index) => {
-      return data[index] || { name: '', problemNum: 0 };
+      return data[index] || { name: '', problemNum: 0, height: 50 };
     });
+
+  const getColor = (problemNum: number) => {
+    switch (problemNum) {
+      case 0:
+        return '#292A2F';
+      case 1:
+      case 2:
+      case 3:
+        return '#DCFFE4';
+      case 4:
+      case 5:
+      case 6:
+        return '#B7FFC7';
+      case 7:
+      case 8:
+      case 9:
+        return '#7DFF99';
+      case 10:
+      case 11:
+      case 12:
+        return '#59FF7E';
+      case 13:
+        return '#08FF3F';
+      default:
+        return '#08FF3F';
+    }
+  };
 
   return (
     <ResponsiveContainer
@@ -36,12 +65,11 @@ const FollowerCurrentGraph = ({ users }: FollowerCurrentGraphProps) => {
         barCategoryGap={30}
         barGap={18}
       >
-        <Bar
-          dataKey="problemNum"
-          radius={[30, 30, 0, 0]}
-          fill="#59FF7E"
-          isAnimationActive={false}
-        />
+        <Bar dataKey="height" radius={[30, 30, 0, 0]} isAnimationActive={false}>
+          {filledData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={getColor(entry.problemNum)} />
+          ))}
+        </Bar>
       </BarChart>
     </ResponsiveContainer>
   );
