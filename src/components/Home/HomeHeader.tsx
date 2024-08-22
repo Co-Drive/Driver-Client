66e23fe1c696f7130e6ad8340f9112736d6d34c1
Seung taek Oh <elqkddl@naver.com>
2024-08-22 11:29:17 +0900

    chore: 필요없는 주석 삭제 및 수정

--- src/components/Home/HomeHeader.tsx
+++ src/components/Home/HomeHeader.tsx
@@ -21,7 +21,6 @@ const HomeHeader = () => {
         </NickNameContainer>
         <PharseContainer>
           <Pharse>오늘도 문제 풀어 볼까요?</Pharse>
-
           <Button onClick={handleRegister}>
             <IcContainer>
               <IcWorkBookBlack />
@@ -39,8 +38,6 @@ export default HomeHeader;
 const Article = styled.article`
   width: 100%;
 
-  /* height: 18.8rem; */
-
   border-radius: 1.2rem;
 
   max-width: 92.6rem;
@@ -48,14 +45,12 @@ const Article = styled.article`
 `;
 
 const Header = styled.header`
-  /* background-color: red; */
-  padding: 3.2rem 3.4rem 3.4rem 3.6rem;
+  padding: 3.2rem 3.4rem 4.4rem 3.6rem;
 `;
 
 const Date = styled.p`
   margin-bottom: 2.9rem;
 
-  /* background-color: red; */
   ${({ theme }) => theme.fonts.body_ligth_12}
   color: ${({ theme }) => theme.colors.white};
 `;
@@ -65,30 +60,27 @@ const NickNameContainer = styled.div`
 
   margin-bottom: 1.6rem;
 
-  /* background-color: coral; */
   color: ${({ theme }) => theme.colors.white};
 `;
 
 const NickName = styled.p`
   margin-left: 0.6rem;
 
-  /* background-color: aqua; */
   color: ${({ theme }) => theme.colors.codrive_green};
 `;
 
 const Pharse = styled.p`
   ${({ theme }) => theme.fonts.title_bold_24};
-  /* margin-right: 41.9rem; */
-
-  background-color: black;
   color: ${({ theme }) => theme.colors.white};
 `;
 
 const PharseContainer = styled.div`
   display: flex;
-  gap: 41.9rem;
 
-  /* background-color: brown; */
+  /* 여기 질문 */
+
+  /* gap: 41.9rem; */
+  justify-content: space-between;
 `;
 
 const Button = styled.button`
@@ -102,14 +94,10 @@ const Button = styled.button`
   max-width: 20rem;
 
   background-color: ${({ theme }) => theme.colors.codrive_green};
-
-  /* background-color: bisque; */
 `;
 
 const IcContainer = styled.div`
   margin-right: 0.6rem;
-
-  /* background-color: cornflowerblue; */
 `;
 
 const Title = styled.div`
