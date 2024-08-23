695b17eb5d13865ed19043df6949423db9bfe2b1
seoAreum <1971236@hansung.ac.kr>
2024-08-23 18:26:17 +0900

    feat: 신청하기 버튼 구현

--- src/page/GroupDetail.tsx
+++ src/page/GroupDetail.tsx
@@ -10,6 +10,8 @@ const GroupDetail = () => {
         <Header />
         <GroupImg src="https://blog.kakaocdn.net/dn/cYnypO/btrzcSaVpNa/VwDfLj2yOWZDKpAhZIlYJ1/img.jpg" />
         <GroupInfo />
+
+        <ApplyBtn type="button">신청하기</ApplyBtn>
       </GroupDetailContainer>
     </PageLayout>
   );
@@ -21,6 +23,7 @@ const GroupDetailContainer = styled.section`
   display: flex;
   justify-content: center;
   flex-direction: column;
+  position: relative;
 
   width: 100%;
   padding: 6.4rem 41.4rem 23.2rem;
@@ -35,4 +38,19 @@ const GroupImg = styled.img`
   border-radius: 1.6rem;
 
   object-fit: cover;
+`;
+
+const ApplyBtn = styled.button`
+  position: fixed;
+  top: 84.4rem;
+  left: 48.3rem;
+
+  padding: 1.8rem 19.5rem;
+
+  box-shadow: rgb(183 255 199 / 70%) 0 0 1.5rem;
+
+  border-radius: 1.6rem;
+  background-color: ${({ theme }) => theme.colors.codrive_green};
+  color: ${({ theme }) => theme.colors.gray900};
+  ${({ theme }) => theme.fonts.title_bold_24};
 `;
