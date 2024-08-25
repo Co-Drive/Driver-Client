b5687985136c41432b61e478a5238753c5080c53
seoAreum <1971236@hansung.ac.kr>
2024-08-26 03:13:53 +0900

    fix: 엔드포인트 잘못된 부분 수정

--- src/libs/apis/Follower/getFollowerSummary.ts
+++ src/libs/apis/Follower/getFollowerSummary.ts
@@ -4,9 +4,10 @@ import { api } from '../../api';
 const getFollowerSummary = async ({
   sortType,
   groupId,
+  page,
 }: GetFollowerSummaryProps) => {
   const { data } = await api.get(
-    `/follow/followings/summary/${sortType}${groupId && `/groupId=${groupId}`}`
+    `/follow/followings/summary/${sortType === '최신순' ? `NEW` : `DICT`}?page=${page}${groupId && `&groupId=${groupId}`}`
   );
 
   return data;
