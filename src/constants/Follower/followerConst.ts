27d70eaa2d3963b79bb6c848c256ebfd37674bbd
seoAreum <1971236@hansung.ac.kr>
2024-08-26 03:47:02 +0900

    Revert "remove: 불필요한 더미데이터 삭제"
    This reverts commit 52a31f8cd7dfb5150b764f36bf3859e77d3ddcfc.

---
+++ src/constants/Follower/followerConst.ts
@@ -0,0 +1,111 @@
+export const FOLLOWER_DUMMY = {
+  id: 3,
+  profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
+  nickname: '일이삼사오육칠팔구십',
+  isFollowed: false,
+  language: 'Javascript',
+  introduce: '안녕하세요 코딩하는 풀스택 개발자 어쩌구 입니다릴릴릴',
+  github: 'https://github.com/Arooming',
+  rate: 75,
+  group: [
+    {
+      id: 0,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디1',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+    {
+      id: 1,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRolslbPtm3OymbdIwYgusZ9wqH4-fvxhln2A&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디1',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+    {
+      id: 2,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwwTi48CyZZGovLP7-u6CPqTn8ygolt2N5aA&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디2',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+    {
+      id: 3,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgQdMSF0UfUeev25_EDlcSS0jCfciYTLT-qw&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디3',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+    {
+      id: 4,
+      imgSrc:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRolslbPtm3OymbdIwYgusZ9wqH4-fvxhln2A&s',
+      title: '이제는 더이상 물러날 곳이 없는 스터디4',
+      tags: ['#swift', '#java', '#javascript'],
+      introduce:
+        '안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다설명칸은 두줄까지 가능합니다 안녕하세요 스터디입니다 설명칸은 두줄까지 가능합니다',
+    },
+  ],
+  recommend: [
+    {
+      id: 10,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflIJYye-3WuwXfwuKLvVcajyyi8Rbu9Bx0g&s',
+      nickname: '코딩하는 갱얼쥐1',
+      language: 'javascript',
+      github: 'https://github.com/Arooming',
+      isFollowed: true,
+    },
+    {
+      id: 11,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflIJYye-3WuwXfwuKLvVcajyyi8Rbu9Bx0g&s',
+      nickname: '코딩하는 갱얼쥐2',
+      language: 'javascript',
+      isFollowed: false,
+    },
+    {
+      id: 12,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflIJYye-3WuwXfwuKLvVcajyyi8Rbu9Bx0g&s',
+      nickname: '코딩하는 갱얼쥐3',
+      language: 'javascript',
+      github: 'https://github.com/Arooming',
+      isFollowed: false,
+    },
+    {
+      id: 13,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflIJYye-3WuwXfwuKLvVcajyyi8Rbu9Bx0g&s',
+      nickname: '코딩하는 갱얼쥐4',
+      language: 'javascript',
+      github: 'https://github.com/Arooming',
+      isFollowed: true,
+    },
+    {
+      id: 14,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflIJYye-3WuwXfwuKLvVcajyyi8Rbu9Bx0g&s',
+      nickname: '코딩하는 갱얼쥐5',
+      language: 'javascript',
+      isFollowed: true,
+    },
+    {
+      id: 15,
+      profileImg:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQflIJYye-3WuwXfwuKLvVcajyyi8Rbu9Bx0g&s',
+      nickname: '코딩하는 갱얼쥐6',
+      language: 'javascript',
+      github: 'https://github.com/Arooming',
+      isFollowed: false,
+    },
+  ],
+};
