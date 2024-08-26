cdde4f408f288f9f2554df4b6e4bc87786b7b853
kimmoonju-102 <kimmun3549@naver.com>
2024-08-26 20:05:02 +0900

    feat: selectBox 안에 range slider컴포넌트 추가 및 구현

--- src/components/GroupAll/groupFilter/LanguageSelectBox.tsx
+++ src/components/GroupAll/groupFilter/LanguageSelectBox.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { IcArrowBottomGray, IcArrowTopGray, IcFilter } from '../../../assets';
 import CommonHashTag from '../../../common/CommonHashTag';
 import { LanguageSectionProps } from '../../../types/GroupCreate/GroupCreateType';
+import RangeSliderFilter from './RangeSlidrFilter';
 
 const ALL_TAG = 'ALL';
 const firstRowTags = ['Python', 'Java', 'JavaScript', 'C++', 'C', 'C#'];
@@ -13,8 +14,21 @@ const LanguageSelectBox = ({
   setSelectedTags,
 }: LanguageSectionProps) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [minValue, setMinValue] = useState<number>(1);
+  const [maxValue, setMaxValue] = useState<number>(50);
+
+  const [tempMinValue, setTempMinValue] = useState<number>(1);
+  const [tempMaxValue, setTempMaxValue] = useState<number>(50);
+
+  // 변경: 값이 설정된 후에만 슬라이더 값을 표시
+  const hasSelectedTagsOrRange =
+    selectedTags.length > 0 || minValue !== 1 || maxValue !== 50;
 
   const toggleDropdown = () => {
+    if (isDropdownOpen) {
+      setMinValue(tempMinValue);
+      setMaxValue(tempMaxValue);
+    }
     setIsDropdownOpen((prev) => !prev);
   };
 
@@ -32,10 +46,6 @@ const LanguageSelectBox = ({
           : [...selectedTags, tag];
 
         setSelectedTags(newTags);
-
-        if (newTags.length >= 3) {
-          toggleDropdown();
-        }
       }
     }
   };
@@ -52,8 +62,6 @@ const LanguageSelectBox = ({
   const selectAllTags = () => {
     const allTags = [...firstRowTags, ...secondRowTags];
     setSelectedTags(allTags);
-
-    toggleDropdown();
   };
 
   return (
@@ -63,28 +71,37 @@ const LanguageSelectBox = ({
           <FilterIconContainer>
             <IcFilter />
           </FilterIconContainer>
-          {selectedTags.length ===
-          firstRowTags.length + secondRowTags.length ? (
-            <SelectedTagContainer key={ALL_TAG}>
-              <CommonHashTag
-                selectedTag={ALL_TAG}
-                removeTag={() => removeTag(ALL_TAG)}
-              />
-            </SelectedTagContainer>
-          ) : selectedTags.length === 0 ? (
-            <DropdownText>그룹 필터</DropdownText>
-          ) : (
-            selectedTags.map((tag, index) => (
-              <SelectedTagContainer key={index}>
+          <SelectedTagsContainer>
+            {selectedTags.length ===
+            firstRowTags.length + secondRowTags.length ? (
+              <SelectedTagContainer key={ALL_TAG}>
                 <CommonHashTag
-                  selectedTag={tag}
-                  removeTag={(e) => {
-                    if (e) e.stopPropagation();
-                    removeTag(tag);
-                  }}
+                  selectedTag={ALL_TAG}
+                  removeTag={() => removeTag(ALL_TAG)}
                 />
               </SelectedTagContainer>
-            ))
+            ) : selectedTags.length === 0 ? (
+              <DropdownText>그룹 필터</DropdownText>
+            ) : (
+              selectedTags.map((tag, index) => (
+                <SelectedTagContainer key={index}>
+                  <CommonHashTag
+                    selectedTag={tag}
+                    removeTag={(e) => {
+                      if (e) e.stopPropagation();
+                      removeTag(tag);
+                    }}
+                  />
+                </SelectedTagContainer>
+              ))
+            )}
+          </SelectedTagsContainer>
+          <Borderline />
+          {/* 변경: 슬라이더 값이 설정된 경우에만 표시 */}
+          {hasSelectedTagsOrRange && (
+            <RangeValue>
+              {minValue}명 - {maxValue}명
+            </RangeValue>
           )}
         </SelectContainer>
         <IconContainer>
@@ -137,6 +154,19 @@ const LanguageSelectBox = ({
               </DropdownItem>
             </AllTagContainer>
           </TagContainer>
+          <RangeSliderContainer>
+            <RangeTitle>수용인원</RangeTitle>
+            <RangeSliderFilter
+              min={0}
+              max={50}
+              step={5}
+              rangeMin={5}
+              minValue={tempMinValue}
+              maxValue={tempMaxValue}
+              setMinValue={setTempMinValue}
+              setMaxValue={setTempMaxValue}
+            />
+          </RangeSliderContainer>
         </DropdownItemContainer>
       )}
     </Section>
@@ -175,8 +205,19 @@ const FilterIconContainer = styled.p`
   margin-right: 1.2rem;
 `;
 
+const SelectedTagsContainer = styled.div`
+  width: 28.6rem;
+
+  border-right: 0.1rem solid ${({ theme }) => theme.colors.gray400};
+`;
+
 const SelectedTagContainer = styled.div`
   display: inline-flex;
+`;
+
+const RangeValue = styled.p`
+  ${({ theme }) => theme.fonts.body_medium_16};
+  color: ${({ theme }) => theme.colors.white};
 `;
 
 const DropdownText = styled.p`
@@ -193,6 +234,7 @@ const IconContainer = styled.div`
 
 const DropdownItemContainer = styled.div`
   width: 100%;
+  height: 27.6rem;
   padding: 1.6rem;
 
   border-radius: 0.8rem;
@@ -218,6 +260,17 @@ const Info = styled.p`
 
 const TagContainer = styled.div`
   display: flex;
+`;
+
+const RangeSliderContainer = styled.div`
+  margin-top: 3.4rem;
+`;
+
+const RangeTitle = styled.p`
+  margin-bottom: 1.5rem;
+
+  ${({ theme }) => theme.fonts.body_medium_16};
+  color: ${({ theme }) => theme.colors.white};
 `;
 
 const LanguageItemsContainer = styled.div`
@@ -251,7 +304,7 @@ const DropdownItem = styled.div<{ $isSelected: boolean }>`
 const Borderline = styled.div`
   margin-left: 1.8rem;
 
-  border-right: 0.1rem solid ${({ theme }) => theme.colors.gray600};
+  border-right: 0.1rem solid ${({ theme }) => theme.colors.gray400};
 `;
 
 const AllTagContainer = styled.div`
