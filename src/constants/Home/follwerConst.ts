7f07318dc9a17092b81a768a070f87e4b3074fd1
Seung taek Oh <elqkddl@naver.com>
2024-08-28 03:10:59 +0900

    chore: 상수 추가

--- src/constants/Home/follwerConst.ts
+++ src/constants/Home/follwerConst.ts
@@ -7,5 +7,26 @@ export const Home_FOLLOWER_DUMMY = {
       nickname: '닉네임은총열글자입니다',
       language: 'JavaScript',
     },
+    {
+      userId: 2,
+      successRate: 40,
+      profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
+      nickname: '나나나나나노노노너너너',
+      language: 'JavaScript',
+    },
+    {
+      userId: 3,
+      successRate: 50,
+      profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
+      nickname: '닉네임은총열글자입니다',
+      language: 'JavaScript',
+    },
+    {
+      userId: 4,
+      successRate: 60,
+      profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
+      nickname: '바이바이바이바이바이',
+      language: 'JavaScript',
+    },
   ],
 };
