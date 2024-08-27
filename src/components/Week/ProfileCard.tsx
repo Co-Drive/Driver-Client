af923cc05992faae74c5771a6cdaa41bd30793a0
Seung taek Oh <elqkddl@naver.com>
2024-08-28 03:06:49 +0900

    style: 프로필 사진 및 원 사이즈 변경

--- src/components/Week/ProfileCard.tsx
+++ src/components/Week/ProfileCard.tsx
@@ -19,12 +19,11 @@ const CustomLabel = ({ profileImg }: CustomLabelProps) => {
   return (
     <image
       href={profileImg}
-      x={10}
-      y={10}
-      width={80}
-      height={80}
+      x={8}
+      y={8}
+      width={60}
+      height={60}
       clipPath="circle(50%)" // 원형으로 자르기
-      // key={userId}
     />
   );
 };
@@ -44,73 +43,61 @@ const HomeProfileCard = ({ user }: HomeProfileCardProps) => {
 
   return (
     <>
-      <CardContainer>
-        <FollowerContainer>
-          <PieChart width={100} height={100}>
-            <Pie
-              dataKey="value"
-              data={chartData}
-              startAngle={90}
-              endAngle={-270}
-              innerRadius={30}
-              outerRadius={50}
-              paddingAngle={0}
-              cornerRadius={15}
-              fill="#494B53"
-              stroke="none"
-            ></Pie>
-            <Pie
-              data={chartData}
-              dataKey="value"
-              endAngle={90 - (360 * successRate) / 100}
-              startAngle={90}
-              outerRadius={50}
-              innerRadius={40}
-              paddingAngle={0}
-              stroke="none"
-              cornerRadius={15}
-            >
-              <Cell key="success" fill="#BF57FF" />
-              <Label
-                content={<CustomLabel profileImg={profileImg} />}
-                position="center"
-              />
-            </Pie>
-          </PieChart>
-          <Text>
-            <p>{NickName}</p>
-          </Text>
-          <LanguageText>#{language}</LanguageText>
-        </FollowerContainer>
-      </CardContainer>
+      <FollowerContainer key={userId}>
+        <PieChart width={76} height={76}>
+          <Pie
+            dataKey="value"
+            data={chartData}
+            startAngle={90}
+            endAngle={-270}
+            innerRadius={33}
+            outerRadius={38}
+            paddingAngle={0}
+            cornerRadius={15}
+            fill="#494B53"
+            stroke="none"
+          ></Pie>
+          <Pie
+            data={chartData}
+            dataKey="value"
+            endAngle={90 - (360 * successRate) / 100}
+            startAngle={90}
+            outerRadius={38}
+            innerRadius={33}
+            paddingAngle={0}
+            stroke="none"
+            cornerRadius={15}
+          >
+            <Cell key="success" fill="#BF57FF" />
+            <Label
+              content={<CustomLabel profileImg={profileImg} />}
+              position="center"
+            />
+          </Pie>
+        </PieChart>
+        <Text>{NickName}</Text>
+        <LanguageText>#{language}</LanguageText>
+      </FollowerContainer>
     </>
   );
 };
 
 export default HomeProfileCard;
 
-const CardContainer = styled.div`
+const FollowerContainer = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
   flex-direction: column;
 
-  width: 38.6rem;
-  padding: 0 1.7rem;
-  margin-top: 4rem;
-
-  background-color: pink;
-  max-width: 38.6rem;
-`;
-
-const FollowerContainer = styled.div`
-  background-color: cadetblue;
+  /* background-color: cadetblue; */
 `;
 
 const Text = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  margin-top: 2.4rem;
 
   ${({ theme }) => theme.fonts.title_bold_16};
   color: ${({ theme }) => theme.colors.white};
@@ -122,6 +109,8 @@ const LanguageText = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  margin-top: 0.8rem;
 
   color: ${({ theme }) => theme.colors.gray400};
   ${({ theme }) => theme.fonts.body_eng_regular_14};
