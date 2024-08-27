a81983d9140556f2cb14ed9bed2cb762702a52e8
Seung taek Oh <elqkddl@naver.com>
2024-08-27 16:00:10 +0900

    feat: 더미데이터 추가

---
+++ src/constants/Home/follwerConst.ts
@@ -0,0 +1,11 @@
+export const Home_FOLLOWER_DUMMY = {
+  followings: [
+    {
+      userId: 1,
+      successRate: 30,
+      profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
+      nickname: '닉네임',
+      language: 'JavaScript',
+    },
+  ],
+};
