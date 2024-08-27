019f4a51471156f03538405b149b38e7f1210277
Seung taek Oh <elqkddl@naver.com>
2024-08-28 01:52:30 +0900

    chore: 상수변경

--- src/constants/Home/follwerConst.ts
+++ src/constants/Home/follwerConst.ts
@@ -4,7 +4,7 @@ export const Home_FOLLOWER_DUMMY = {
       userId: 1,
       successRate: 30,
       profileImg: 'https://avatars.githubusercontent.com/u/80264647?v=4',
-      nickname: '닉네임',
+      nickname: '닉네임은총열글자입니다',
       language: 'JavaScript',
     },
   ],
