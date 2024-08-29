d5c3be17f242cdccfc9da93eb54b98d4d2d64f0b
seoAreum <1971236@hansung.ac.kr>
2024-08-30 07:14:58 +0900

    feat: 관리자 뷰 퍼블리싱

---
+++ src/components/Admin/AdminMemberList.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import CommonUserList from '../../common/CommonUserList';
+import MemberHeader from '../GroupMember/MemberHeader';
+
+const AdminMemberList = () => {
+  const { id } = useParams();
+  if (!id) return;
+  const groupId = parseInt(id);
+
+  const [sorting, setSorting] = useState('최신순');
+
+  const handleClickSorting = (
+    e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
+  ) => {
+    const { innerHTML } = e.currentTarget;
+    setSorting(innerHTML);
+  };
+  return (
+    <AdminListContainer>
+      <MemberHeader
+        isAdmin={true}
+        sorting={sorting}
+        handleClickSorting={handleClickSorting}
+      />
+      <CommonUserList
+        isAdmin={true}
+        sorting={sorting}
+        selectedGroupId={groupId}
+        isFollowerList={false}
+      />
+    </AdminListContainer>
+  );
+};
+
+export default AdminMemberList;
+
+const AdminListContainer = styled.article`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  margin-top: 5.8rem;
+`;
