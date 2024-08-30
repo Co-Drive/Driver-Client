94497f69c5f804a5122a1d0a8ab2b8f0c92cd16b
Seung taek Oh <elqkddl@naver.com>
2024-08-30 15:56:16 +0900

    feat: 객체분해할당 처리

--- src/libs/hooks/Home/useGetUserAchieve.ts
+++ src/libs/hooks/Home/useGetUserAchieve.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import getUserAchieve from '../../apis/Home/getUserAchieve';
 
 const useGetUserAchieve = () => {
-  const data = useQuery({
+  const { data } = useQuery({
     queryKey: ['get-user-achieve'],
     queryFn: async () => await getUserAchieve(),
   });
