d100f8d33e1f4ceec7eafe7ec7af1f123484f228
seoAreum <1971236@hansung.ac.kr>
2024-08-30 12:13:11 +0900

    feat: 그룹 상태 변경 및 선택된 그룹의 정보를 가져오는 api 연결

--- src/components/Admin/GroupStatistics/GroupStatistics.tsx
+++ src/components/Admin/GroupStatistics/GroupStatistics.tsx
@@ -1,74 +1,83 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import useGetRoomInfo from '../../../libs/hooks/Admin/useGetRoomInfo';
+import usePatchRoomStatus from '../../../libs/hooks/Admin/usePatchRoomStatus';
 import GroupStatus from './GroupStatus';
 import NumOfLanguages from './NumOfLanguages';
 import NumOfMembers from './NumOfMembers';
 
 const GroupStatistics = () => {
-  const roomStatus = 'ACTIVE';
-  const memberCount = 15;
-  const capacity = 20;
-  const approvedCount = 10;
-  const requestedCount = 30;
-  const languageMemberCount = [
-    {
-      language: 'Java',
-      count: 5,
-    },
-    {
-      language: 'Javascript',
-      count: 16,
-    },
-    {
-      language: 'Python',
-      count: 3,
-    },
-    {
-      language: 'Ruby',
-      count: 20,
-    },
-    {
-      language: 'C++',
-      count: 17,
-    },
-  ];
+  const { id } = useParams();
+  if (!id) return;
+  const roomId = parseInt(id);
 
-  const [clickedStatus, setClickedStatus] = useState(
-    roomStatus === 'ACTIVE'
-      ? '모집 중'
-      : roomStatus === 'INACTIVE'
-        ? '모집 마감'
-        : '활동 종료'
-  );
+  const { data, isLoading } = useGetRoomInfo({ roomId });
+  const {
+    roomStatus,
+    memberCount,
+    capacity,
+    approvedCount,
+    requestedCount,
+    languageMemberCount,
+  } = !isLoading && data?.data;
+
+  const [clickedStatus, setClickedStatus] = useState('');
+
+  const { mutation } = usePatchRoomStatus();
 
   const handleClickStatus = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    e.stopPropagation();
-
     const { innerText } = e.currentTarget;
     setClickedStatus(innerText);
+
+    const clickedStatusEN =
+      innerText === '모집 중'
+        ? 'ACTIVE'
+        : innerText === '모집 마감'
+          ? 'INACTIVE'
+          : 'CLOSED';
+
+    if (clickedStatusEN !== roomStatus) {
+      mutation({ roomId, status: clickedStatusEN });
+    }
   };
 
+  useEffect(() => {
+    const newStatus =
+      roomStatus === 'ACTIVE'
+        ? '모집 중'
+        : roomStatus === 'INACTIVE'
+          ? '모집 마감'
+          : '활동 종료';
+
+    setClickedStatus(newStatus);
+  }, [roomStatus]);
+
   return (
-    <GroupStatisticsContainer>
-      <GroupStatus
-        clickedStatus={clickedStatus}
-        handleClickStatus={handleClickStatus}
-      />
-      <NumOfMembers
-        numOfMembers={{
-          memberCount: memberCount,
-          capacity: capacity,
-          approvedCount: approvedCount,
-          requestedCount: requestedCount,
-        }}
-      />
-      <NumOfLanguages
-        languageMemberCount={languageMemberCount}
-        capacity={capacity}
-      />
-    </GroupStatisticsContainer>
+    <>
+      {!isLoading && (
+        <GroupStatisticsContainer>
+          <GroupStatus
+            clickedStatus={clickedStatus}
+            handleClickStatus={handleClickStatus}
+          />
+          <NumOfMembers
+            numOfMembers={{
+              memberCount: memberCount,
+              capacity: capacity,
+              approvedCount: approvedCount,
+              requestedCount: requestedCount,
+            }}
+          />
+          <NumOfLanguages
+            languageMemberCount={languageMemberCount}
+            capacity={capacity}
+          />
+        </GroupStatisticsContainer>
+      )}
+    </>
   );
 };
 
