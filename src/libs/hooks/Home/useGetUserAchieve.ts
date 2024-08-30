c6689e38951903b6af3d099a7bc1f0d8227575c9
Seung taek Oh <elqkddl@naver.com>
2024-08-30 14:45:34 +0900

    feat: 오늘의 문제풀이 API 생성

---
+++ src/libs/hooks/Home/useGetUserAchieve.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import getUserAchieve from '../../apis/Home/getUserAchieve';
+
+const useGetUserAchieve = () => {
+  const data = useQuery({
+    queryKey: ['get-user-achieve'],
+    queryFn: async () => await getUserAchieve(),
+  });
+
+  return data;
+};
+
+export default useGetUserAchieve;
