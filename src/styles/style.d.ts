abbe32786a989664b2790a14ba69e11235957379
kimmoonju-102 <kimmun3549@naver.com>
2024-08-31 17:16:58 +0900

    style: 폰트 추가

--- src/styles/style.d.ts
+++ src/styles/style.d.ts
@@ -40,6 +40,7 @@ declare module 'styled-components' {
       title_medium_20: SerializedStyles;
       title_regular_20: SerializedStyles;
       title_regular_14: SerializedStyles;
+      title_bold_46: SerializedStyles;
 
       // body
       body_medium_20: SerializedStyles;
