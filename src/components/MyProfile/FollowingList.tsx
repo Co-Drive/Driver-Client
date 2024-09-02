9f1f2fd1b6620e35f0bd590221cb8064c6922b57
kimmoonju-102 <kimmun3549@naver.com>
2024-09-02 18:25:33 +0900

    chore: 불필요한 코드 삭제

--- src/components/MyProfile/FollowingList.tsx
+++ src/components/MyProfile/FollowingList.tsx
@@ -7,7 +7,6 @@ import {
 } from '../../assets';
 import useUpdateFollower from '../../libs/hooks/Follower/useUpdateFollower';
 import { UpdateFollowerProps } from '../../types/Follower/Personal/personalType';
-import PageLayout from '../PageLayout/PageLayout';
 
 const FollowingList = () => {
   // 더미 데이터 생성
@@ -52,13 +51,61 @@ const FollowingList = () => {
       githubUrl: '',
       isFollowing: true,
     },
+    {
+      userId: 6,
+      profileImg: '',
+      nickname: '팔로잉유저2',
+      language: 'TypeScript',
+      githubUrl: '',
+      isFollowing: true,
+    },
+    {
+      userId: 7,
+      profileImg: '',
+      nickname: '팔로잉유저2',
+      language: 'TypeScript',
+      githubUrl: '',
+      isFollowing: true,
+    },
+    {
+      userId: 8,
+      profileImg: '',
+      nickname: '팔로잉유저2',
+      language: 'TypeScript',
+      githubUrl: '',
+      isFollowing: true,
+    },
+    {
+      userId: 9,
+      profileImg: '',
+      nickname: '팔로잉유저2',
+      language: 'TypeScript',
+      githubUrl: '',
+      isFollowing: true,
+    },
+    {
+      userId: 10,
+      profileImg: '',
+      nickname: '팔로잉유저2',
+      language: 'TypeScript',
+      githubUrl: '',
+      isFollowing: true,
+    },
+    {
+      userId: 11,
+      profileImg: '',
+      nickname: '팔로잉유저2',
+      language: 'TypeScript',
+      githubUrl: '',
+      isFollowing: true,
+    },
   ];
 
   // 팔로워와 팔로잉 분리
   const followers = dummyData.filter((user) => !user.isFollowing);
   const followings = dummyData.filter((user) => user.isFollowing);
 
-  const [isFollowerSelected, setIsFollowerSelected] = useState(true); // 팔로워 탭이 기본 선택
+  const [isFollowerSelected, setIsFollowerSelected] = useState(true);
 
   const { mutation } = useUpdateFollower();
 
@@ -72,107 +119,111 @@ const FollowingList = () => {
   };
 
   return (
-    <PageLayout category="홈">
-      <FollowContainer>
-        <HeaderContainer>
-          <TitleContainer>
-            <Title>친구 목록</Title>
-            <FriendCount>
-              {isFollowerSelected ? followers.length : followings.length}명
-            </FriendCount>
-          </TitleContainer>
-          <TabContainer>
-            <Tab
-              $selected={isFollowerSelected}
-              onClick={() => setIsFollowerSelected(true)}
-            >
-              팔로워
-            </Tab>
-            <Divider>|</Divider>
-            <Tab
-              $selected={!isFollowerSelected}
-              onClick={() => setIsFollowerSelected(false)}
-            >
-              팔로잉
-            </Tab>
-          </TabContainer>
-        </HeaderContainer>
-        <RecommendCard>
-          {(isFollowerSelected ? followers : followings).map(
-            (
-              user: {
-                userId: number;
-                profileImg: string;
-                nickname: string;
-                language: string;
-                githubUrl: string;
-                isFollowing: boolean;
-              },
-              idx: number
-            ) => {
-              const {
-                userId,
-                profileImg,
-                nickname,
-                language,
-                githubUrl,
-                isFollowing,
-              } = user;
-              return (
-                <PersonalCard key={userId} $addHr={idx < 4}>
-                  <ProfileImg
-                    src={profileImg}
-                    $isGithubExit={githubUrl?.length !== 0}
-                  />
-                  {githubUrl && (
-                    <IcContainer>
-                      <IcGithubLogoSmall />
-                    </IcContainer>
-                  )}
+    <FollowContainer>
+      <HeaderContainer>
+        <TitleContainer>
+          <Title>친구 목록</Title>
+          <FriendCount>
+            {isFollowerSelected ? followers.length : followings.length}명
+          </FriendCount>
+        </TitleContainer>
+        <TabContainer>
+          <Tab
+            $selected={isFollowerSelected}
+            onClick={() => setIsFollowerSelected(true)}
+          >
+            팔로워
+          </Tab>
+          <Divider>|</Divider>
+          <Tab
+            $selected={!isFollowerSelected}
+            onClick={() => setIsFollowerSelected(false)}
+          >
+            팔로잉
+          </Tab>
+        </TabContainer>
+      </HeaderContainer>
+      <RecommendCard>
+        {(isFollowerSelected ? followers : followings).map(
+          (
+            user: {
+              userId: number;
+              profileImg: string;
+              nickname: string;
+              language: string;
+              githubUrl: string;
+              isFollowing: boolean;
+            },
+            idx: number
+          ) => {
+            const {
+              userId,
+              profileImg,
+              nickname,
+              language,
+              githubUrl,
+              isFollowing,
+            } = user;
+            return (
+              <PersonalCard key={userId} $addHr={idx < 4}>
+                <ProfileImg
+                  src={profileImg}
+                  $isGithubExit={githubUrl?.length !== 0}
+                />
+                {githubUrl && (
+                  <IcContainer>
+                    <IcGithubLogoSmall />
+                  </IcContainer>
+                )}
 
-                  <ProfileInfo>
-                    <Nickname>{nickname}</Nickname>
-                    <Language>{`#${language}`}</Language>
-                  </ProfileInfo>
+                <ProfileInfo>
+                  <Nickname>{nickname}</Nickname>
+                  <Language>{`#${language}`}</Language>
+                </ProfileInfo>
 
-                  <FollowingBtn
-                    type="button"
-                    $isFollowed={isFollowing}
-                    onClick={() =>
-                      handleClickFollowerBtn({
-                        nickname,
-                        isDelete: isFollowing,
-                      })
-                    }
-                  >
-                    {isFollowing ? <IcFollowingGray /> : <IcUnfollowingWhite />}
-                    <FollowingText $isFollowed={isFollowing}>
-                      {isFollowing ? `팔로잉` : `팔로우`}
-                    </FollowingText>
-                  </FollowingBtn>
-                </PersonalCard>
-              );
-            }
-          )}
-        </RecommendCard>
-      </FollowContainer>
-    </PageLayout>
+                <FollowingBtn
+                  type="button"
+                  $isFollowed={isFollowing}
+                  onClick={() =>
+                    handleClickFollowerBtn({
+                      nickname,
+                      isDelete: isFollowing,
+                    })
+                  }
+                >
+                  {isFollowing ? <IcFollowingGray /> : <IcUnfollowingWhite />}
+                  <FollowingText $isFollowed={isFollowing}>
+                    {isFollowing ? `팔로잉` : `팔로우`}
+                  </FollowingText>
+                </FollowingBtn>
+              </PersonalCard>
+            );
+          }
+        )}
+      </RecommendCard>
+      <DeleteIdContainer>
+        <DeleteText>코드라이브를 더이상 이용하지 않는다면</DeleteText>
+        <DeleteBtn>회원탈퇴</DeleteBtn>
+      </DeleteIdContainer>
+    </FollowContainer>
   );
 };
 
-// Tab 스타일링
-// 스타일 컴포넌트 정의
+const FollowContainer = styled.article``;
+
 const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 
-  margin-bottom: 16px;
+  margin-bottom: 2.8rem;
 `;
 
 const TitleContainer = styled.div`
   display: flex;
   align-items: center;
+
+  margin-left: 0.2rem;
 `;
 
 const Title = styled.h2`
@@ -209,21 +260,25 @@ const Divider = styled.div`
   color: #666;
 `;
 
-const FollowContainer = styled.article`
-  margin-top: 4rem;
-`;
-
 const RecommendCard = styled.article`
   display: grid;
   gap: 0.4rem 1.8rem;
   grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
 
   width: 100%;
   padding: 2rem;
+  margin-bottom: 5.6rem;
 
   border-radius: 1.6rem;
   background-color: ${({ theme }) => theme.colors.gray800};
+
+  overflow-y: auto;
+  max-height: 44rem;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const PersonalCard = styled.article<{ $addHr: boolean }>`
@@ -300,4 +355,19 @@ const FollowingText = styled.p<{ $isFollowed: boolean }>`
   ${({ theme }) => theme.fonts.title_bold_16};
 `;
 
+const DeleteIdContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+`;
+
+const DeleteText = styled.p`
+  color: ${({ theme }) => theme.colors.gray300};
+  ${({ theme }) => theme.fonts.body_eng_medium_12};
+`;
+
+const DeleteBtn = styled.button`
+  color: ${({ theme }) => theme.colors.codrive_purple};
+  ${({ theme }) => theme.fonts.body_eng_medium_12};
+`;
 export default FollowingList;
