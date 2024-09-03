1162dc369944a347ec3f4b84b86679fc0d325a55
Seung taek Oh <elqkddl@naver.com>
2024-09-04 02:04:28 +0900

    feat: isLoading을 이용한 데이터 패칭

--- src/common/CommonMonthSolve.tsx
+++ src/common/CommonMonthSolve.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import CommonCalendar from '../components/Calendar/Calendar';
 import useGetMonthSolve from '../libs/hooks/Home/useGetMonthSolve';
@@ -7,31 +7,16 @@ const CommonMonthSolve = () => {
   const currentMonth = new Date().getMonth() + 1; // getMonth()는 0부터 시작하므로 +1 필요
   const monthText = `${currentMonth}월 문제풀이 현황`;
 
-  const [stats, setStats] = useState({
-    totalCount: 0,
-    longestPeriod: 0,
-    maxCount: 0,
-  });
-
   const year = new Date().getFullYear();
   const [clickedYear, setClickedYear] = useState(year);
   const [clickedMonth, setClickedMonth] = useState(currentMonth);
 
-  const { data } = useGetMonthSolve({
+  const { data, isLoading } = useGetMonthSolve({
     year: clickedYear,
     month: clickedMonth,
   });
 
-  useEffect(() => {
-    if (data) {
-      const { totalCount, longestPeriod, maxCount } = data.data;
-      setStats({
-        totalCount,
-        longestPeriod,
-        maxCount,
-      });
-    }
-  }, [data]);
+  const { totalCount, longestPeriod, maxCount } = !isLoading && data?.data;
 
   return (
     <WeekRateContainer>
@@ -39,15 +24,15 @@ const CommonMonthSolve = () => {
         <Month>{monthText}</Month>
         <SolveContainer>
           <SolveCount>
-            {stats.totalCount}
+            {totalCount}
             <SolveCountText>문제</SolveCountText>
           </SolveCount>
         </SolveContainer>
         <LongestSolve>
-          최장 문제 풀이 기간 <Text>{stats.longestPeriod}</Text>일
+          최장 문제 풀이 기간 <Text>{longestPeriod}</Text>일
         </LongestSolve>
         <LongestSolve>
-          최대 문제 풀이 개수 <Text>{stats.maxCount}</Text>개
+          최대 문제 풀이 개수 <Text>{maxCount}</Text>개
         </LongestSolve>
       </div>
 
