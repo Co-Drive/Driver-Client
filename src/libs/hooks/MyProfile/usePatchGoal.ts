1726f2746edde478a687d65594de3a96498674a6
seoAreum <1971236@hansung.ac.kr>
2024-09-06 23:55:53 +0900

    feat: 목표 수정 에러 핸들링

--- src/libs/hooks/MyProfile/usePatchGoal.ts
+++ src/libs/hooks/MyProfile/usePatchGoal.ts
@@ -1,18 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
 import patchGoal from '../../apis/MyProfile/patchGoal';
 
 const usePatchGoal = (saveGoal: () => void) => {
-  const queryClient = useQueryClient();
+  const [errMsg, setErrMsg] = useState('');
 
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async (goal: number) => await patchGoal(goal),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['get-goal'] });
       saveGoal();
     },
+    onError: (err: { response: { data: { message: string } } }) => {
+      const { message } = err.response.data;
+      setErrMsg(message);
+    },
   });
 
-  return { mutation: mutation.mutate };
+  return { mutation: mutation.mutate, patchGoalErr: errMsg };
 };
 
 export default usePatchGoal;
