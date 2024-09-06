1d276a13203868faa03a2b05eddc6df50bfa73ad
seoAreum <1971236@hansung.ac.kr>
2024-09-07 01:39:35 +0900

    feat: 그룹 상태 변경 에러 핸들링

--- src/libs/hooks/Admin/usePatchRoomStatus.ts
+++ src/libs/hooks/Admin/usePatchRoomStatus.ts
@@ -1,19 +1,25 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
 import { PatchRoomStatusProps } from '../../../types/Admin/adminType';
 import patchRoomStatus from '../../apis/Admin/patchRoomStatus';
 
 const usePatchRoomStatus = () => {
-  const queryClient = useQueryClient();
+  const [errMsg, setErrMsg] = useState('');
 
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async ({ roomId, status }: PatchRoomStatusProps) =>
       await patchRoomStatus({ roomId, status }),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['get-room-info'] });
     },
+    onError: (err: { response: { data: { message: string } } }) => {
+      const { message } = err.response.data;
+      setErrMsg(message);
+    },
   });
 
-  return { mutation: mutation.mutate };
+  return { mutation: mutation.mutate, patchRoomErr: errMsg };
 };
 
 export default usePatchRoomStatus;
