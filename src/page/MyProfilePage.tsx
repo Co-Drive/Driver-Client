8933b0dff82a06da2122082e2185602397f83bc3
seoAreum <1971236@hansung.ac.kr>
2024-09-09 14:07:16 +0900

    feat: 유효하지 않은 주소 차단

--- src/page/MyProfilePage.tsx
+++ src/page/MyProfilePage.tsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import ErrorModal from '../common/Modal/ErrorModal/ErrorModal';
 import FollowingList from '../components/MyProfile/FollowingList';
 import MyGoal from '../components/MyProfile/MyGoal';
 import Profile from '../components/MyProfile/Profile';
 import PageLayout from '../components/PageLayout/PageLayout';
 
 const MyProfilePage = () => {
+  const accessUrl = useParams();
+  const { id } = accessUrl;
+  const username = sessionStorage.getItem('name');
+
+  const [errModalOn, setErrModalOn] = useState(false);
+
+  useEffect(() => {
+    if (id !== username) {
+      setErrModalOn(true);
+    }
+  }, [id]);
   return (
     <PageLayout category={'홈'}>
-      <MyProfileContainer>
-        <Profile />
-        <MyGoal />
-        <FollowingList />
-      </MyProfileContainer>
+      {errModalOn ? (
+        <ErrorModal
+          errMsg="유효하지 않은 주소입니다."
+          onClose={() => setErrModalOn(false)}
+          callbackPage="/"
+        />
+      ) : (
+        <MyProfileContainer>
+          <Profile />
+          <MyGoal />
+          <FollowingList />
+        </MyProfileContainer>
+      )}
     </PageLayout>
   );
 };
