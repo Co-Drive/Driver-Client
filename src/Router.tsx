a259e993a60541155193495b83371982d913be9f
Seung taek Oh <elqkddl@naver.com>
2024-10-07 15:55:51 +0900

    feat: 테스트 라우터 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -18,7 +18,6 @@ import RegisterPage from './page/RegisterPage';
 import SolutionListPage from './page/SolutionListPage';
 import SolutionPage from './page/SolutionPage';
 import SolvePage from './page/SolvePage';
-import TestPage from './page/TestPage';
 
 const Router = () => {
   return (
@@ -45,7 +44,6 @@ const Router = () => {
         <Route path="/oauth/github/callback" element={<LoginLoadingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/group-all" element={<GroupAllPage />} />
-        <Route path="/test" element={<TestPage />} />
       </Routes>
     </BrowserRouter>
   );
