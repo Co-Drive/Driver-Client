35979cb4fc939aac242d23e7ce482dbb101730b7
kimmoonju-102 <kimmun3549@naver.com>
2024-10-13 17:00:56 +0900

    feat: 리포지토리 컴포넌트 퍼블

---
+++ src/components/Register/Repositories.tsx
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import CommonInput from '../../common/CommonInput';
+interface RepositoriesProps {
+  repositories: string;
+  changeRepositories: {
+    isExistNickname: boolean;
+    isClickedCheckBtn: boolean;
+  };
+  handleChangeInputs: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleRepositoriesCheck: () => void;
+}
+
+const Repositories = ({
+  repositories,
+  changeRepositories,
+  handleChangeInputs,
+  handleRepositoriesCheck,
+}: RepositoriesProps) => {
+  const { isExitedRepositories, isClickedCheckRepositoriesBtn } =
+    changeRepositories;
+
+  return (
+    <RepositoriesContainer>
+      <TitleContainer>
+        <Title>깃허브 리포지토리</Title>
+        <Essential>*</Essential>
+      </TitleContainer>
+      <Info>코드라이브와 연동할 깃허브 리포지토리의 이름을 작성해주세요</Info>
+      <InputWrapper>
+        <CommonInput
+          category="repositories"
+          value={repositories}
+          isClickedCheckRepositoriesBtn={isClickedCheckRepositoriesBtn}
+          isExitedRepositories={isExitedRepositories}
+          handleChangeInputs={handleChangeInputs}
+        />
+        <Button type="button" onClick={handleRepositoriesCheck}>
+          검색
+        </Button>
+      </InputWrapper>
+    </RepositoriesContainer>
+  );
+};
+
+export default Repositories;
+
+const RepositoriesContainer = styled.div`
+  max-height: 6.6rem;
+
+  margin-bottom: 9.7rem;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 0.6rem;
+  margin-left: 0.2rem;
+`;
+
+const Title = styled.h2`
+  color: ${({ theme }) => theme.colors.white};
+
+  ${({ theme }) => theme.fonts.title_bold_20};
+`;
+
+const Essential = styled.span`
+  margin-left: 0.6rem;
+
+  ${({ theme }) => theme.fonts.title_medium_20};
+  color: ${({ theme }) => theme.colors.codrive_purple};
+`;
+
+const Info = styled.span`
+  display: block;
+
+  margin-bottom: 1.6rem;
+  margin-left: 0.2rem;
+
+  color: ${({ theme }) => theme.colors.gray300};
+  ${({ theme }) => theme.fonts.detail_regular_12};
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  width: 6.4rem;
+  height: 4.8rem;
+  margin-left: 1rem;
+
+  border: none;
+  border-radius: 0.6rem;
+  background-color: ${({ theme }) => theme.colors.codrive_green};
+  color: ${({ theme }) => theme.colors.gray900};
+  ${({ theme }) => theme.fonts.title_bold_16};
+`;
