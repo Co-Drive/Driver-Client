59322b4e458e90804e0c0c91aa195d1bddb2d8be
kimmoonju-102 <kimmun3549@naver.com>
2024-10-17 03:28:39 +0900

    chore: api 파일명 변경

--- src/libs/apis/GroupEdit/getRooms.ts
+++
@@ -1,9 +0,0 @@
-import { api } from '../../api';
-
-const getRooms = async (roomId: number) => {
-  const { data } = await api.get(`/rooms/${roomId}`);
-
-  return data;
-};
-
-export default getRooms;
