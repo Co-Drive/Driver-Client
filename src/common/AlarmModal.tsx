6f21412fa7542b059c06e5cf26e0b6a4a1c85fcf
Seung taek Oh <elqkddl@naver.com>
2024-10-17 15:02:14 +0900

    feat: 호버 시 안에 들어오는 컨텐츠 작업

--- src/common/AlarmModal.tsx
+++ src/common/AlarmModal.tsx
@@ -6,23 +6,47 @@ interface AlarmModalProps {
 }
 
 const AlarmModal = ({ isOpen, handleClose }: AlarmModalProps) => {
-  const ModalList = {
-    Test: [
-      '새로운 알림이 왔습니다',
-      '새로움 알림이 왔어요',
-      '알림이 도착했어요',
+  const ALARMLIST = {
+    NewAlarms: [
+      { groupName: '[그룹이름] ', message: '에 새로운 신청이 들어왔습니다' },
+      { groupName: '[닉네임] ', message: '님이 회원님을 팔로우 했습니다' },
+    ],
+    ReadAlarms: [
+      { groupName: '[그룹이름] ', message: '에 [닉네임] 님이 참여하였습니다' },
+      { groupName: '[그룹이름] ', message: '에 새로운 신청이 들어왔습니다' },
     ],
   };
 
-  const { Test } = ModalList;
+  const { NewAlarms, ReadAlarms } = ALARMLIST;
 
   return (
     <>
       {isOpen && (
         <ModalContainer onMouseLeave={handleClose}>
-          {Test.map((data) => {
-            return <ModalTab key={data}>{data}</ModalTab>;
-          })}
+          <Section>
+            <Title>새로운 알림</Title>
+            {NewAlarms.map((data, idx) => {
+              return (
+                <ModalTab key={idx}>
+                  <Highlight>{data.groupName}</Highlight>
+                  {data.message}
+                </ModalTab>
+              );
+            })}
+          </Section>
+          <Divider />
+          <Section>
+            <Title>읽음</Title>
+
+            {ReadAlarms.map((data, idx) => {
+              return (
+                <ModalTab key={idx}>
+                  <Highlight>{data.groupName}</Highlight>
+                  {data.message}
+                </ModalTab>
+              );
+            })}
+          </Section>
         </ModalContainer>
       )}
     </>
@@ -33,15 +57,48 @@ export default AlarmModal;
 
 const ModalContainer = styled.ul`
   display: flex;
-  gap: 0.4rem;
-  justify-content: center;
   flex-direction: column;
+  position: absolute;
+  top: 3rem;
+  right: -2rem;
+
+  width: 36.4rem;
+  padding: 2.2rem 2.2rem 1.8rem 2rem;
+
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.gray600};
 `;
 
 const ModalTab = styled.li`
   width: 100%;
+  padding: 1.1rem 1rem 1.1rem 0;
+  ${({ theme }) => theme.fonts.title_regular_14};
 
-  background-color: blue;
+  color: ${({ theme }) => theme.colors.white};
+`;
 
-  text-align: center;
+const Title = styled.p`
+  margin-bottom: 0.8rem;
+
+  color: ${({ theme }) => theme.colors.white};
+
+  ${({ theme }) => theme.fonts.detail_regular_12};
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Divider = styled.div`
+  width: calc(100% + 4.2rem); /* ModalContainer의 100% 너비에 4.2rem 추가 */
+  height: 0.1rem;
+  margin: 1.2rem -2rem;
+
+  background-color: ${({ theme }) => theme.colors.gray500};
+`;
+
+const Highlight = styled.span`
+  ${({ theme }) => theme.fonts.title_semiBold_14};
+  color: ${({ theme }) => theme.colors.white};
 `;
