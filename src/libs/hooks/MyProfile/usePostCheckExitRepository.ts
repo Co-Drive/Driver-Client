106769cfbd0333671152a4dca81567ddf945e504
kimmoonju-102 <kimmun3549@naver.com>
2024-10-18 23:29:23 +0900

    chore: Props명 수정

--- src/libs/hooks/MyProfile/usePostCheckExitRepository.ts
+++ src/libs/hooks/MyProfile/usePostCheckExitRepository.ts
@@ -2,17 +2,17 @@ import { useMutation } from '@tanstack/react-query';
 import postCheckExitRepository from '../../apis/MyProfile/postCheckExitRepository';
 
 const usePostCheckExitRepository = (
-  updateIsExitRepository: (isExit: boolean) => void
+  updateIsExitRepositories: (isExit: boolean) => void
 ) => {
   const mutation = useMutation({
     mutationFn: async (githubRepositoryName: string) =>
       await postCheckExitRepository(githubRepositoryName),
     onSuccess: () => {
-      updateIsExitRepository(false);
+      updateIsExitRepositories(false);
     },
     onError: (err: { response: { data: { code: number } } }) => {
       const { code } = err.response.data;
-      if (code === 409) updateIsExitRepository(true);
+      if (code === 404) updateIsExitRepositories(true);
     },
   });
 
