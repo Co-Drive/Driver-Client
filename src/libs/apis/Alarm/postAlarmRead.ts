4f131886e857737316843a8660a95e05abc7785a
Seung taek Oh <elqkddl@naver.com>
2024-10-22 23:42:24 +0900

    chore: 주석 설명 추가

--- src/libs/apis/Alarm/postAlarmRead.ts
+++ src/libs/apis/Alarm/postAlarmRead.ts
@@ -2,6 +2,7 @@ import { api } from '../../api';
 
 const postAlarmRead = async (notificationIds: number) => {
   const { data } = await api.post('/notifications/read', {
+    // requestBody에서 배열로 받아옴
     notificationIds: [notificationIds],
   });
 
