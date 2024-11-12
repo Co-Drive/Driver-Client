d466af62953f2f2f290fcea7f165f1ea7299221c
seoAreum <1971236@hansung.ac.kr>
2024-11-12 14:52:36 +0900

    chore: 타입 분리

--- src/types/Solution/solutionTypes.ts
+++ src/types/Solution/solutionTypes.ts
@@ -19,16 +19,24 @@ export interface SolutionHeaderTopProps {
   paintedStarArr: Array<number>;
 }
 
+export interface SavedSolutionListProps {
+  userId: number;
+  isSmallList: boolean;
+  handleDisabledMoreBtn?: (value: boolean) => void;
+}
+
+export interface recordType {
+  recordId: number;
+  title: string;
+  level: number;
+  tags: Array<string>;
+  platform: string;
+  problemUrl: string;
+  createdAt: string;
+}
+
 export interface SavedSolutionProps {
-  record: {
-    recordId: number;
-    title: string;
-    level: number;
-    tags: Array<string>;
-    platform: string;
-    problemUrl: string;
-    createdAt: string;
-  };
+  record: recordType;
   followerId?: number;
   clickedPage?: number;
   isModal?: boolean;
