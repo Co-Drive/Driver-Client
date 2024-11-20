b2aa63eab4b8fbc5b4da1d963a76d84a2e345997
seoAreum <1971236@hansung.ac.kr>
2024-11-21 02:25:38 +0900

    style: 푸터가 생기면서 변경된 레이아웃 반영

--- src/page/LoadingPage.tsx
+++ src/page/LoadingPage.tsx
@@ -20,13 +20,14 @@ export default LoadingPage;
 
 const LoadingPageContainer = styled.section<{ $isPageLoading?: boolean }>`
   display: flex;
-  justify-content: center;
+  justify-content: ${({ $isPageLoading }) => !$isPageLoading && 'center'};
   align-items: center;
   flex-direction: column;
 
   width: 100vw;
   height: ${({ $isPageLoading }) =>
     $isPageLoading ? 'calc(100vh - 11.6rem)' : '100vh'};
+  padding-top: ${({ $isPageLoading }) => $isPageLoading && '11.6rem'};
 `;
 
 const LottieContainer = styled.article`
