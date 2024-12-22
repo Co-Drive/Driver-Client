eb294e1b48df81c4eaa9836db958065b8eadcdc0
seoAreum <1971236@hansung.ac.kr>
2024-12-22 23:14:09 +0900

    chore: sentry 설치 및 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    sentryVitePlugin({
+      org: 'rooming-03',
+      project: 'javascript-react',
+      sourcemaps: {
+        assets: './dist/**',
+        filesToDeleteAfterUpload: './dist/**/*.map', // Sentry에 sourcemap이 업로드된 후에 해당 경로의 파일 제거
+      },
+    }),
+    sentryVitePlugin({
+      org: 'rooming-03',
+      project: 'javascript-react',
+    }),
+  ],
+
   server: {
     host: 'localhost',
     port: 3000,
     open: true,
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
