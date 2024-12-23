3ff7b66bb15223e7407f0ab675252a4a464b8ec9
seoAreum <1971236@hansung.ac.kr>
2024-12-23 17:47:18 +0900

    chore: 터미널 warning 반영

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,11 @@ export default defineConfig({
     sentryVitePlugin({
       org: 'rooming-03',
       project: 'javascript-react',
+      telemetry: false, // 텔레메트리 데이터 전송 비활성화
       sourcemaps: {
         assets: './dist/**',
         filesToDeleteAfterUpload: './dist/**/*.map', // Sentry에 sourcemap이 업로드된 후에 해당 경로의 파일 제거
       },
-    }),
-    sentryVitePlugin({
-      org: 'rooming-03',
-      project: 'javascript-react',
     }),
   ],
 
