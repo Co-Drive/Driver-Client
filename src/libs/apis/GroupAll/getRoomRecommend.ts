456c73b3187404eaacf78a35354a4f22c6053ff0
kimmoonju-102 <kimmun3549@naver.com>
2025-01-26 16:18:41 +0900

    fix: 불필요한 옵셔널 체이닝 삭제

--- src/libs/apis/GroupAll/getRoomRecommend.ts
+++ src/libs/apis/GroupAll/getRoomRecommend.ts
@@ -1,6 +1,6 @@
 import { api } from '../../api';
 
-export const getRoomRecommend = async (userId?: number) => {
+export const getRoomRecommend = async (userId: number) => {
   const { data } = await api.get(`/rooms/${userId}/recommend`);
 
   return data;
--- src/libs/hooks/GroupAll/useGetRoomRecommend.ts
+++ src/libs/hooks/GroupAll/useGetRoomRecommend.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { getRoomRecommend } from '../../apis/GroupAll/getRoomRecommend';
 
-const useGetRoomRecommend = (userId?: number) => {
+const useGetRoomRecommend = (userId: number) => {
   const { data, isLoading } = useQuery({
     queryKey: ['get-room-recommend', userId],
     queryFn: async () => await getRoomRecommend(userId),
