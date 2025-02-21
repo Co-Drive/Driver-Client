915acaa965c83fbd81d641caa5a1e35ea967730f
seoAreum <1971236@hansung.ac.kr>
2025-02-21 21:14:29 +0900

    feat: 유저 리스트 관련 공통 컴포넌트 > query string으로 선택된 정렬 관리

--- src/utils/handleClickPage.ts
+++ src/utils/handleClickPage.ts
@@ -2,28 +2,32 @@ import { SetURLSearchParams } from 'react-router-dom';
 
 type clickedPageType = {
   clickedPage: number;
+  sorting: string;
   setSearchParams: SetURLSearchParams;
 };
 
 export const handleClickPrevBtn = ({
   clickedPage,
+  sorting,
   setSearchParams,
 }: clickedPageType) => {
   const prevPage = (clickedPage - 1).toString();
-  setSearchParams({ page: prevPage });
+  setSearchParams({ page: prevPage, sort: sorting });
 };
 
 export const handleClickPage = ({
   clickedPage,
+  sorting,
   setSearchParams,
 }: clickedPageType) => {
-  setSearchParams({ page: clickedPage.toString() });
+  setSearchParams({ page: clickedPage.toString(), sort: sorting });
 };
 
 export const handleClickNextBtn = ({
   clickedPage,
+  sorting,
   setSearchParams,
 }: clickedPageType) => {
   const nextPage = (clickedPage + 1).toString();
-  setSearchParams({ page: nextPage });
+  setSearchParams({ page: nextPage, sort: sorting });
 };
