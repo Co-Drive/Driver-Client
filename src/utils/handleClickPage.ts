b9a95d9d12f51021cf71f04dbdca6adab91ed4a4
seoAreum <1971236@hansung.ac.kr>
2025-03-02 20:56:21 +0900

    feat: 함수 유틸화

--- src/utils/handleClickPage.ts
+++ src/utils/handleClickPage.ts
@@ -6,16 +6,26 @@ type clickedPageType = {
   setSearchParams: SetURLSearchParams;
 };
 
+const updateParams = ({
+  clickedPage,
+  sorting,
+  setSearchParams,
+}: clickedPageType) => {
+  const page = clickedPage.toString();
+
+  // sorting이 null이거나 undefined인 경우 false로 처리
+  sorting?.trim()
+    ? setSearchParams({ page, sort: sorting })
+    : setSearchParams({ page });
+};
+
 export const handleClickPrevBtn = ({
   clickedPage,
   sorting,
   setSearchParams,
 }: clickedPageType) => {
-  const prevPage = (clickedPage - 1).toString();
-
-  sorting === null
-    ? setSearchParams({ page: prevPage })
-    : setSearchParams({ page: prevPage, sort: sorting });
+  const prevPage = clickedPage - 1;
+  updateParams({ clickedPage: prevPage, sorting, setSearchParams });
 };
 
 export const handleClickPage = ({
@@ -23,10 +33,7 @@ export const handleClickPage = ({
   sorting,
   setSearchParams,
 }: clickedPageType) => {
-  const page = clickedPage.toString();
-  sorting === null
-    ? setSearchParams({ page })
-    : setSearchParams({ page, sort: sorting });
+  updateParams({ clickedPage, sorting, setSearchParams });
 };
 
 export const handleClickNextBtn = ({
@@ -34,8 +41,6 @@ export const handleClickNextBtn = ({
   sorting,
   setSearchParams,
 }: clickedPageType) => {
-  const nextPage = (clickedPage + 1).toString();
-  sorting === null
-    ? setSearchParams({ page: nextPage })
-    : setSearchParams({ page: nextPage, sort: sorting });
+  const nextPage = clickedPage + 1;
+  updateParams({ clickedPage: nextPage, sorting, setSearchParams });
 };
